85db5b03a1fb5c2544559774b2f37f2ab108e1e4
Arundathi Patil <arundpil@in.ibm.com>
2019-01-17 14:10:08 +0530

    Seed code for loopback server
    Added seed project structure for loopback server. This will act as
    intermediate layer between UI and backend. The angular UI app will be
    serverd through this server.
    
    Issue-ID: CCSDK-943
    Change-Id: I03259782927eda0d7e5bff989f61a36f09a3afd8
    Signed-off-by: Arundathi Patil <arundpil@in.ibm.com>

---
+++ cds-ui/server/index.js
@@ -0,0 +1,42 @@
+/*
+============LICENSE_START==========================================
+===================================================================
+Copyright (C) 2018-19 IBM Intellectual Property. All rights reserved.
+===================================================================
+
+Unless otherwise specified, all software contained herein is licensed
+under the Apache License, Version 2.0 (the License);
+you may not use this software except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+============LICENSE_END============================================
+*/
+
+const application = require('./dist');
+
+module.exports = application;
+
+if (require.main === module) {
+  // Run the application
+  const config = {
+    rest: {
+      port: +process.env.PORT || 3000,
+      host: process.env.HOST || 'localhost',
+      openApiSpec: {
+        // useful when used with OASGraph to locate your application
+        setServersFromRequest: true,
+      },
+    },
+  };
+  application.main(config).catch(err => {
+    console.error('Cannot start the application.', err);
+    process.exit(1);
+  });
+}
