83b8b91a3648b585d3fd451f99aab90db5fe5fc9
Singal, Kapil (ks220y) <ks220y@att.com>
2019-01-20 17:49:52 -0500

    Resource Resolution Service: Source Rest
    Complex Data Processing for Primary-Config-Data DataSource Resource Resolution Service
    
    Change-Id: I62492b5c4e3c0b831f9027df5d42c6b80186debc
    Issue-ID: CCSDK-674
    Signed-off-by: Singal, Kapil (ks220y) <ks220y@att.com>

--- components/resource-dict/src/test/java/org/onap/ccsdk/apps/controllerblueprints/resource/dict/factory/ResourceSourceMappingFactoryTest.java
+++ components/resource-dict/src/test/java/org/onap/ccsdk/apps/controllerblueprints/resource/dict/factory/ResourceSourceMappingFactoryTest.java
@@ -28,7 +28,7 @@ public class ResourceSourceMappingFactoryTest {
         ResourceSourceMappingFactory.INSTANCE.registerSourceMapping("primary-db", "source-primary-db");
         ResourceSourceMappingFactory.INSTANCE.registerSourceMapping("input", "source-input");
         ResourceSourceMappingFactory.INSTANCE.registerSourceMapping("default", "source-default");
-        ResourceSourceMappingFactory.INSTANCE.registerSourceMapping("mdsal", "source-rest");
+        ResourceSourceMappingFactory.INSTANCE.registerSourceMapping("primary-config-data", "source-rest");
 
         String nodeTypeName = ResourceSourceMappingFactory.INSTANCE.getRegisterSourceMapping("primary-db");
         Assert.notNull(nodeTypeName, "Failed to get primary-db mapping");
