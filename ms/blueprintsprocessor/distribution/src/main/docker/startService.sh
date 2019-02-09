45b260c81451f74bfd763186d5ff42e777077d2b
Alexis de Talhouët <adetalhouet89@gmail.com>
2019-02-09 18:33:55 -0500

    Multiple fixes & enhancement
    - Rework docker image build
    - Fix compress function
    - Fix node type name for source-primary-db
    
    Change-Id: Ide38e17036e76047d9f850b714ba0da4add5cd9d
    Issue-ID: CCSDK-414
    Signed-off-by: Alexis de Talhouët <adetalhouet89@gmail.com>

--- ms/blueprintsprocessor/distribution/src/main/docker/startService.sh
+++ ms/blueprintsprocessor/distribution/src/main/docker/startService.sh
@@ -5,5 +5,4 @@ nodeName=BlueprintsProcessor_1.0.0_$(cat /proc/self/cgroup | grep docker | sed s
 echo "APP Config HOME : ${APP_CONFIG_HOME}"
 export APP_HOME=/opt/app/onap
 
-mv -rf /config /opt/app/onap/
 source /etc/run.source
