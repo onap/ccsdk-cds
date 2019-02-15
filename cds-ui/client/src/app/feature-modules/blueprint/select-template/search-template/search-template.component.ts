836235eac4162a3c5ee33ea91b75c4c879151474
Ezhilarasi <ezhrajam@in.ibm.com>
2019-02-15 22:13:05 +0530

    Search template file to json
    Convert input file to json and save it as Blueprint model
    Change-Id: I10ab6ea26f3ef9af589960930f6f442b479a1050
    Issue-ID: CCSDK-703
    Signed-off-by: Ezhilarasi <ezhrajam@in.ibm.com>

--- cds-ui/client/src/app/feature-modules/blueprint/select-template/search-template/search-template.component.ts
+++ cds-ui/client/src/app/feature-modules/blueprint/select-template/search-template/search-template.component.ts
@@ -19,7 +19,9 @@ limitations under the License.
 ============LICENSE_END============================================
 */
 
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { IBlueprint} from '../../../../common/core/store/models/blueprint.model';
 
 @Component({
   selector: 'app-search-template',
@@ -27,10 +29,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-template.component.scss']
 })
 export class SearchTemplateComponent implements OnInit {
-
+  file: any;
+  localBluePrintData: IBlueprint;
+  fileText: object[];
+  
   constructor() { }
 
   ngOnInit() {
   }
 
+  fileChanged(e: any) {
+    this.file = e.target.files[0];
+    let fileReader = new FileReader();
+    fileReader.readAsText(e.srcElement.files[0]);
+    var me = this;
+    fileReader.onload = function () {
+      let fileData = JSON.stringify(fileReader.result);
+      me.localBluePrintData = JSON.parse(fileData);
+      console.log(me.localBluePrintData);
+    }
+  }
 }
