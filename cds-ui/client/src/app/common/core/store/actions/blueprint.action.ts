ef4d79b4cde4cec2ab65bca841efa9ec23a63676
Arundathi Patil <arundpil@in.ibm.com>
2019-03-05 16:10:24 +0530

    Ngrx store changes
    Made model changes as file upload type is changed to .zip
    
    Issue-ID: CCSDK-1112
    Change-Id: Idf206ee05fba99b0a6bafcc3867f16025a8486f4
    Signed-off-by: Arundathi Patil <arundpil@in.ibm.com>

--- cds-ui/client/src/app/common/core/store/actions/blueprint.action.ts
+++ cds-ui/client/src/app/common/core/store/actions/blueprint.action.ts
@@ -18,6 +18,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ============LICENSE_END============================================
 */
+
 import { Injectable } from '@angular/core';
 import { Action, Store } from '@ngrx/store';
 import { IBlueprint } from '../models/blueprint.model';
@@ -36,6 +37,8 @@ export const SAVE_BLUEPRINT_FAILURE = 'SAVE_BLUEPRINT_FAILURE';
 export const SET_BLUEPRINT = 'SET Blueprint';
 export const REMOVE_BLUEPRINT = 'Remove Blueprint';
 
+export const SET_BLUEPRINT_STATE = 'SET Blueprint state';
+
 
 export class LoadBlueprint implements Action {
     readonly type = LOAD_BLUEPRINT;
@@ -50,6 +53,11 @@ export class LoadBlueprintSuccess implements Action {
 export class LoadBlueprintFailure implements Action {
     readonly type = LOAD_BLUEPRINT_FAILURE;
     constructor(public error: any) {}
+}
+
+export class SetBlueprintState implements Action {
+    readonly type = SET_BLUEPRINT_STATE;
+    constructor(public payload: IBlueprintState) {}
 }
 
 // export class SetBlueprint implements Action {
@@ -67,4 +75,4 @@ export class UpdateBlueprint implements Action {
     constructor(public payload: IBlueprint) {}
 }
 
-export type Actions = LoadBlueprint | LoadBlueprintSuccess | LoadBlueprintFailure;
+export type Actions = LoadBlueprint | LoadBlueprintSuccess | LoadBlueprintFailure | SetBlueprintState;
