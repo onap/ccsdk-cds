5e8208fc77ccac998c6eea74b490a6e07c3c511a
ottero <rodrigo.ottero@est.tech>
2019-03-26 11:54:26 +0000

    Implementing the requested improvements
    Addressing comments done in https://gerrit.onap.org/r/#/c/82315/ and in the subsequent CDS weekly call.
    
    (Resubmitting patch done on previous repo, https://gerrit.onap.org/r/#/c/83063/)
    
    Change-Id: I6f948dedf972c33ea6fe0f65884f23cd48adcfba
    Issue-ID: CCSDK-926
    Signed-off-by: ottero <rodrigo.ottero@est.tech>

--- ms/blueprintsprocessor/modules/commons/rest-lib/src/main/kotlin/org/onap/ccsdk/cds/blueprintsprocessor/rest/service/BasicAuthRestClientService.kt
+++ ms/blueprintsprocessor/modules/commons/rest-lib/src/main/kotlin/org/onap/ccsdk/cds/blueprintsprocessor/rest/service/BasicAuthRestClientService.kt
@@ -12,8 +12,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * SPDX-License-Identifier: Apache-2.0
  */
 
 package org.onap.ccsdk.cds.blueprintsprocessor.rest.service
