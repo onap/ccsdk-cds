6c7ccfa2e3d344796c3b6ed8d84f00eba58b434a
Ezhilarasi <ezhrajam@in.ibm.com>
2019-04-01 20:15:16 +0530

    Added controller methods to Blueprint
    Change-Id: I43b55e9f7baba5eb0322edc0d43e4e138156f50d
    Issue-ID: CCSDK-1068
    Signed-off-by: Ezhilarasi <ezhrajam@in.ibm.com>

--- cds-ui/server/src/controllers/blueprint-rest.controller.ts
+++ cds-ui/server/src/controllers/blueprint-rest.controller.ts
@@ -38,123 +38,82 @@ import {
   put,
   del,
   requestBody,
+  Response,
+  RestBindings,
 } from '@loopback/rest';
-import {Blueprint} from '../models';
-import {BlueprintRepository} from '../repositories';
+import { inject } from '@loopback/core';
+import { Blueprint } from '../models';
+import { BlueprintRepository } from '../repositories';
+import { Request } from 'express-serve-static-core';
+import * as FormData from 'form-data';
+import * as Formidable from 'formidable';
+import * as fs from 'fs';
+
 
 export class BlueprintRestController {
+  authtoken: any;
+  formData: FormData = new FormData();
+  contentType: string;
   constructor(
     @repository(BlueprintRepository)
-    public blueprintRepository : BlueprintRepository,
-  ) {}
+    public blueprintRepository: BlueprintRepository,
+    @inject('services.BlueprintService') public bpservice: BlueprintService
+  ) { }
 
   @post('/blueprints', {
     responses: {
       '200': {
         description: 'Blueprint model instance',
-        content: {'application/json': {schema: {'x-ts-type': Blueprint}}},
+        content: { 'application/json': { schema: { 'x-ts-type': Blueprint } } },
       },
     },
   })
-  async create(@requestBody() blueprint: Blueprint): Promise<Blueprint> {
-    return await this.blueprintRepository.create(blueprint);
-  }
-
-  @get('/blueprints/count', {
-    responses: {
-      '200': {
-        description: 'Blueprint model count',
-        content: {'application/json': {schema: CountSchema}},
+  async create(@requestBody({
+    description: 'multipart/form-data value.',
+    required: true,
+    content: {
+      'multipart/form-data': {
+        'x-parser': 'stream',
+        schema: { type: 'object' },
       },
     },
-  })
-  async count(
-    @param.query.object('where', getWhereSchemaFor(Blueprint)) where?: Where,
-  ): Promise<Count> {
-    return await this.blueprintRepository.count(where);
+  }) request: Request,
+    @inject(RestBindings.Http.RESPONSE) response: Response): Promise<object> {
+    this.authtoken = request.headers.authorization;
+    var form = new Formidable.IncomingForm();
+    form.multiples = true;
+    form.parse(request, (err, fields, files) => {
+      let filedat = this.getMapValue(files, 'file');
+      fs.rename(filedat.path, form.uploadDir + filedat.name, (err) => {
+        if (err) throw err;
+      });
+      let fileToSend = form.uploadDir + filedat.name;
+      this.formData.append('file', fs.createReadStream(fileToSend), { filename: 'cba.zip' });
+      this.contentType = "multipart/form-data; boundary=" + this.formData.getBoundary();
+      console.log(this.contentType);
+    });
+    return this.bpservice.saveblueprint(this.contentType, this.authtoken, this.formData);
   }
-
   @get('/blueprints', {
     responses: {
       '200': {
         description: 'Array of Blueprint model instances',
         content: {
           'application/json': {
-            schema: {type: 'array', items: {'x-ts-type': Blueprint}},
+            schema: { type: 'array', items: { 'x-ts-type': Blueprint } },
           },
         },
       },
     },
   })
-  async find(
-    @param.query.object('filter', getFilterSchemaFor(Blueprint)) filter?: Filter,
-  ): Promise<Blueprint[]> {
-    return await this.blueprintRepository.find(filter);
+  async getAllBlueprint(): Promise<any> {
+    return await this.bpservice.getAllblueprints();;
   }
 
-  @patch('/blueprints', {
-    responses: {
-      '200': {
-        description: 'Blueprint PATCH success count',
-        content: {'application/json': {schema: CountSchema}},
-      },
-    },
-  })
-  async updateAll(
-    @requestBody() blueprint: Blueprint,
-    @param.query.object('where', getWhereSchemaFor(Blueprint)) where?: Where,
-  ): Promise<Count> {
-    return await this.blueprintRepository.updateAll(blueprint, where);
+  getMapValue(obj: any, key: any) {
+    if (obj.hasOwnProperty(key))
+      return obj[key];
+    throw new Error("Invalid map key.");
   }
 
-  @get('/blueprints/{id}', {
-    responses: {
-      '200': {
-        description: 'Blueprint model instance',
-        content: {'application/json': {schema: {'x-ts-type': Blueprint}}},
-      },
-    },
-  })
-  async findById(@param.path.number('id') id: number): Promise<Blueprint> {
-    return await this.blueprintRepository.findById(id);
-  }
-
-  @patch('/blueprints/{id}', {
-    responses: {
-      '204': {
-        description: 'Blueprint PATCH success',
-      },
-    },
-  })
-  async updateById(
-    @param.path.number('id') id: number,
-    @requestBody() blueprint: Blueprint,
-  ): Promise<void> {
-    await this.blueprintRepository.updateById(id, blueprint);
-  }
-
-  @put('/blueprints/{id}', {
-    responses: {
-      '204': {
-        description: 'Blueprint PUT success',
-      },
-    },
-  })
-  async replaceById(
-    @param.path.number('id') id: number,
-    @requestBody() blueprint: Blueprint,
-  ): Promise<void> {
-    await this.blueprintRepository.replaceById(id, blueprint);
-  }
-
-  @del('/blueprints/{id}', {
-    responses: {
-      '204': {
-        description: 'Blueprint DELETE success',
-      },
-    },
-  })
-  async deleteById(@param.path.number('id') id: number): Promise<void> {
-    await this.blueprintRepository.deleteById(id);
-  }
 }
