afa54b3be88019b85ea43cc5ec376d6a04acefd2
prathamesh morde <prathamesh.morde@bell.ca>
2019-04-01 23:19:42 -0400

    CDS-SDC Listener application
    Things done
    -Implementation of GRPC client to store CBA archive using CDS Backend
    -Unit test coverage.
    -Tested locally.
    -Minor bug fix, performance improvement, more logging and make use of BluePrintProcessorHandler api
    in ListenerServiceImpl class.
    -Extract csar artifact from IDistributionClientResult and store it into local file(disk).
    
    Change-Id: I08b0de017396bb76d5bc13ddb9e7b430990e8df0
    Issue-ID: CCSDK-1184
    Signed-off-by: prathamesh morde <prathamesh.morde@bell.ca>

--- ms/cds-sdc-listener/application/src/main/java/org/onap/ccsdk/cds/cdssdclistener/service/ListenerServiceImpl.java
+++ ms/cds-sdc-listener/application/src/main/java/org/onap/ccsdk/cds/cdssdclistener/service/ListenerServiceImpl.java
@@ -8,6 +8,10 @@
 
 package org.onap.ccsdk.cds.cdssdclistener.service;
 
+import static java.nio.file.Files.walk;
+import com.google.protobuf.ByteString;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -17,12 +21,24 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Enumeration;
+import java.util.List;
+import java.util.Optional;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
+import org.apache.commons.io.FileUtils;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.onap.ccsdk.cds.cdssdclistener.client.CdsSdcListenerAuthClientInterceptor;
+import org.onap.ccsdk.cds.cdssdclistener.handler.BluePrintProcesssorHandler;
+import org.onap.ccsdk.cds.controllerblueprints.common.api.Status;
+import org.onap.ccsdk.cds.controllerblueprints.management.api.BluePrintUploadInput;
+import org.onap.ccsdk.cds.controllerblueprints.management.api.FileChunk;
+import org.onap.sdc.api.results.IDistributionClientDownloadResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
@@ -31,13 +47,21 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties("listenerservice")
 public class ListenerServiceImpl implements ListenerService {
 
-    @Value("${listenerservice.config.csarArchive}")
-    private String csarArchivePath;
+    @Autowired
+    private BluePrintProcesssorHandler bluePrintProcesssorHandler;
 
-    @Value("${listenerservice.config.cbaArchive}")
-    private String cbaArchivePath;
+    @Autowired
+    private CdsSdcListenerAuthClientInterceptor cdsSdcListenerAuthClientInterceptor;
+
+    @Value("${listenerservice.config.grpcAddress}")
+    private String grpcAddress;
+
+    @Value("${listenerservice.config.grpcPort}")
+    private int grpcPort;
 
     private static final String CBA_ZIP_PATH = "Artifacts/Resources/[a-zA-Z0-9-_]+/Deployment/CONTROLLER_BLUEPRINT_ARCHIVE/[a-zA-Z0-9-_]+[.]zip";
+    private static final int SUCCESS_CODE = 200;
+    private static final String CSAR_FILE_EXTENSION = ".csar";
     private static final Logger LOGGER = LoggerFactory.getLogger(ListenerServiceImpl.class);
 
     @Override
@@ -50,6 +74,7 @@ public class ListenerServiceImpl implements ListenerService {
                 String fileName = entry.getName();
                 if (Pattern.matches(CBA_ZIP_PATH, fileName)) {
                     final String cbaArchiveName = Paths.get(fileName).getFileName().toString();
+                    LOGGER.info("Storing the CBA archive {}", cbaArchiveName);
                     storeBluePrint(zipFile, cbaArchiveName, cbaStorageDir, entry);
                 }
             }
@@ -59,8 +84,8 @@ public class ListenerServiceImpl implements ListenerService {
     }
 
     private void storeBluePrint(ZipFile zipFile, String fileName, Path cbaArchivePath, ZipEntry entry) {
-        final String changedFileName = fileName + ".zip";
-        Path targetLocation = cbaArchivePath.resolve(changedFileName);
+        Path targetLocation = cbaArchivePath.resolve(fileName);
+        LOGGER.info("The target location for zip file is {}", targetLocation);
         File targetZipFile = new File(targetLocation.toString());
 
         try {
@@ -73,15 +98,43 @@ public class ListenerServiceImpl implements ListenerService {
         try (InputStream inputStream = zipFile.getInputStream(entry); OutputStream out = new FileOutputStream(
             targetZipFile)) {
             IOUtils.copy(inputStream, out);
+            LOGGER.info("Succesfully store the CBA archive {} at this location", targetZipFile);
         } catch (Exception e) {
-            LOGGER.error("Failed to put zip file into target location {}", targetLocation, e);
+            LOGGER.error("Failed to put zip file into target location {}, {}", targetLocation, e);
+        }
+    }
+
+    @Override
+    public void saveBluePrintToCdsDatabase(Path cbaArchivePath) {
+        Optional<List<File>> zipFiles = getZipFilesFromDisk(cbaArchivePath);
+        zipFiles.ifPresent(this::prepareRequestForCdsBackend);
+    }
+
+    @Override
+    public void extractCsarAndStore(IDistributionClientDownloadResult result, String csarArchivePath) {
+
+        // Create CSAR storage directory
+        Path csarStorageDir = getStorageDirectory(csarArchivePath);
+
+        byte[] payload = result.getArtifactPayload();
+        String csarFileName = result.getArtifactFilename() + CSAR_FILE_EXTENSION;
+        Path targetLocation = csarStorageDir.resolve(csarFileName);
+
+        LOGGER.info("The target location for the CSAR file is {}", targetLocation);
+
+        File targetCsarFile = new File(targetLocation.toString());
+
+        try (FileOutputStream outFile = new FileOutputStream(targetCsarFile)) {
+            outFile.write(payload, 0, payload.length);
+        } catch (Exception e) {
+            LOGGER.error("Failed to put CSAR file into target location {}, {}", targetLocation, e);
         }
     }
 
     private Path getStorageDirectory(String path) {
         Path fileStorageLocation = Paths.get(path).toAbsolutePath().normalize();
 
-        if (!Files.exists(fileStorageLocation)) {
+        if (!fileStorageLocation.toFile().exists()) {
             try {
                 return Files.createDirectories(fileStorageLocation);
             } catch (IOException e) {
@@ -91,8 +144,74 @@ public class ListenerServiceImpl implements ListenerService {
         return fileStorageLocation;
     }
 
-    @Override
-    public void saveBluePrintToCdsDatabase(ZipFile file) {
-        //TODO
+    private void prepareRequestForCdsBackend(List<File> files) {
+        final ManagedChannel channel = getManagedChannel();
+
+        files.forEach(zipFile -> {
+            try {
+                final BluePrintUploadInput request = generateBluePrintUploadInputBuilder(zipFile);
+
+                // Send request to CDS Backend.
+                final Status responseStatus = bluePrintProcesssorHandler.sendRequest(request, channel);
+
+                if (responseStatus.getCode() != SUCCESS_CODE) {
+                    LOGGER.error("Failed to store the CBA archive into CDS DB due to {}",
+                        responseStatus.getErrorMessage());
+                } else {
+                    LOGGER.info(responseStatus.getMessage());
+                }
+
+            } catch (Exception e) {
+                LOGGER.error("Failure due to", e);
+            } finally {
+
+                //Delete the file from the local disk.
+                boolean fileDeleted = zipFile.delete();
+
+                if (!fileDeleted) {
+                    LOGGER.error("Could not able to delete the zip file {}", zipFile.toString());
+                }
+            }
+        });
+    }
+
+    private ManagedChannel getManagedChannel() {
+        return ManagedChannelBuilder.forAddress(grpcAddress, grpcPort)
+                                    .usePlaintext()
+                                    .intercept(cdsSdcListenerAuthClientInterceptor)
+                                    .build();
+    }
+
+    private BluePrintUploadInput generateBluePrintUploadInputBuilder(File file) throws IOException {
+        byte[] bytes = FileUtils.readFileToByteArray(file);
+        FileChunk fileChunk = FileChunk.newBuilder().setChunk(ByteString.copyFrom(bytes)).build();
+
+        return BluePrintUploadInput.newBuilder()
+                .setFileChunk(fileChunk)
+                .build();
+    }
+
+    private Optional<List<File>> getZipFilesFromDisk(Path archivePath) {
+        try (Stream<Path> fileTree = walk(archivePath)) {
+            // Get the list of files from the path
+            return Optional.of(fileTree.filter(Files::isRegularFile)
+                                       .map(Path::toFile)
+                                       .collect(Collectors.toList()));
+
+        } catch (IOException e) {
+            LOGGER.error("Failed to find the file", e);
+        }
+
+        return Optional.empty();
+    }
+
+    private Optional<List<File>> getCsarFilesFromDisk(Path path) {
+        try (Stream<Path> fileTree = walk(path)) {
+            // Get the list of files from the path
+            return Optional.of(fileTree.filter(Files::isRegularFile).map(Path::toFile).collect(Collectors.toList()));
+        } catch (IOException e) {
+            LOGGER.error("Failed to find the csar file", e);
+        }
+        return Optional.empty();
     }
 }
--- ms/cds-sdc-listener/application/src/test/java/org/onap/ccsdk/cds/cdssdclistener/service/ListenerServiceImplTest.java
+++ ms/cds-sdc-listener/application/src/test/java/org/onap/ccsdk/cds/cdssdclistener/service/ListenerServiceImplTest.java
@@ -8,23 +8,34 @@
 
 package org.onap.ccsdk.cds.cdssdclistener.service;
 
+import static junit.framework.TestCase.assertTrue;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 import org.junit.runner.RunWith;
+import org.onap.ccsdk.cds.cdssdclistener.client.CdsSdcListenerAuthClientInterceptor;
+import org.onap.ccsdk.cds.cdssdclistener.handler.BluePrintProcesssorHandler;
+import org.onap.sdc.impl.mock.DistributionClientDownloadResultStubImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@EnableConfigurationProperties(ListenerServiceImpl.class)
+@EnableConfigurationProperties({ListenerServiceImpl.class, CdsSdcListenerAuthClientInterceptor.class,
+    BluePrintProcesssorHandler.class})
 @SpringBootTest(classes = {ListenerServiceImplTest.class})
 public class ListenerServiceImplTest {
 
     private static final String CSAR_SAMPLE = "src/test/resources/service-Testsvc140.csar";
+    private static final String ZIP_FILE = ".zip";
+    private static final String CSAR_FILE = ".csar";
+    private String csarArchivePath;
     private Path tempDirectoryPath;
 
     @Rule
@@ -33,12 +44,41 @@ public class ListenerServiceImplTest {
     @Autowired
     private ListenerServiceImpl listenerService;
 
-    @Test
-    public void extractBluePrintSuccessfully() {
-        // Arrange
-        tempDirectoryPath = Paths.get(folder.getRoot().toString(), "cds-sdc-listener-test");
 
+    @Before
+    public void setup() {
+        csarArchivePath = folder.getRoot().toString();
+        tempDirectoryPath = Paths.get(csarArchivePath, "cds-sdc-listener-test");
+    }
+    @Test
+    public void extractBluePrintSuccessfully() throws IOException {
         // Act
         listenerService.extractBluePrint(CSAR_SAMPLE, tempDirectoryPath.toString());
+
+        // Verify
+        String result = checkFileExists(tempDirectoryPath);
+        assertTrue(result.contains(ZIP_FILE));
+    }
+
+    @Test
+    public void storeCsarArtifactToFileSuccessfully() throws  IOException {
+        // Arrange
+        DistributionClientDownloadResultStubImpl resultStub = new DistributionClientDownloadResultStubImpl();
+
+        // Act
+        listenerService.extractCsarAndStore(resultStub, tempDirectoryPath.toString());
+
+        // Verify
+        String result = checkFileExists(tempDirectoryPath);
+        assertTrue(result.contains(CSAR_FILE));
+    }
+
+    private String checkFileExists(Path path) throws IOException {
+        return Files.walk(path)
+            .filter(Files::isRegularFile)
+            .map(Path::toFile)
+            .findAny()
+            .get()
+            .getName();
     }
 }
