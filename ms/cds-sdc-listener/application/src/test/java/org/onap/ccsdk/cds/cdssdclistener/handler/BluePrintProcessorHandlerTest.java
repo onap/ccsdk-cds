b06496bdaac11c5a6ec99e98e1df518be11f0873
prathamesh morde <prathamesh.morde@bell.ca>
2019-04-10 23:00:24 -0400

    CDS-Listener should send response back to SDC
    -Added notification logic so the listener will send response back to SDC.
    (Fail to download, Fail to upload and succesfully uploaded for this conditions
    now)
    -Some bug fixes.
    -Made use of correct copyright header
    
    Change-Id: Iefc108dbdd2bec048fd55630822c99ee1b13bab5
    Issue-ID: CCSDK-1201
    Signed-off-by: prathamesh morde <prathamesh.morde@bell.ca>

--- ms/cds-sdc-listener/application/src/test/java/org/onap/ccsdk/cds/cdssdclistener/handler/BluePrintProcessorHandlerTest.java
+++ ms/cds-sdc-listener/application/src/test/java/org/onap/ccsdk/cds/cdssdclistener/handler/BluePrintProcessorHandlerTest.java
@@ -1,11 +1,18 @@
 /*
- * Copyright (C) 2019 Bell Canada. All rights reserved.
+ * Copyright © 2019 Bell Canada
  *
- * NOTICE:  All the intellectual and technical concepts contained herein are
- * proprietary to Bell Canada and are protected by trade secret or copyright law.
- * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
 package org.onap.ccsdk.cds.cdssdclistener.handler;
 
 import static org.junit.Assert.assertEquals;
@@ -34,7 +41,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
 
 @RunWith(SpringRunner.class)
 @EnableConfigurationProperties({BluePrintProcesssorHandler.class, CdsSdcListenerAuthClientInterceptor.class})
