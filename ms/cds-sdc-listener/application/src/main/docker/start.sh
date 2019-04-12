7d8cc406848ae533df57033ef25260bb3e15b28b
prathamesh morde <prathamesh.morde@bell.ca>
2019-04-12 11:41:36 -0400

    Work in Progress cds-sdc-listener containerization
    Change-Id: Ib99f2c2f7c3f8ad46ec9630a548370e7467a96b4
    Issue-ID: CCSDK-1145
    Signed-off-by: prathamesh morde <prathamesh.morde@bell.ca>

---
+++ ms/cds-sdc-listener/application/src/main/docker/start.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+extraArgs=$@
+java -jar /opt/app/onap/cds-sdc-listener/cds-sdc-listener-distribution.jar \
+-Djava.security.egd=file:/dev/./urandom \
+-Dlistenerservice.config.asdcAddress=${asdcAddress} \
+-Dlistenerservice.config.messageBusAddress=${messageBusAddress} \
+-Dlistenerservice.config.user=${sdcusername} \
+-Dlistenerservice.config.password=${password} \
+-Dlistenerservice.config.pollingInterval=${pollingInterval} \
+-Dlistenerservice.config.pollingTimeout=${pollingTimeout} \
+-Dlistenerservice.config.relevantArtifactTypes=${relevantArtifactTypes} \
+-Dlistenerservice.config.consumerGroup=${consumerGroup} \
+-Dlistenerservice.config.environmentName=${environmentName} \
+-Dlistenerservice.config.consumerId=${consumerId} \
+-Dlistenerservice.config.keyStorePassword=${keyStorePassword} \
+-Dlistenerservice.config.keyStorePath=${keyStorePath} \
+-Dlistenerservice.config.activateServerTLSAuth=${activateServerTLSAuth} \
+-Dlistenerservice.config.isUseHttpsWithDmaap=${isUseHttpsWithDmaap} \
+-Dlistenerservice.config.archivePath=${archivePath} \
+-Dlistenerservice.config.grpcAddress=${grpcAddress} \
+-Dlistenerservice.config.grpcPort=${grpcPort} \
+-Dlistenerservice.config.authHeader=${authHeader} \
+${extraArgs}
