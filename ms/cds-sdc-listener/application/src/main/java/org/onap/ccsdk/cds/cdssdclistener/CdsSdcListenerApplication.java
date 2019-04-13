1dc460c9dd7569d4184353f9a5979ae4190affdd
prathamesh morde <prathamesh.morde@bell.ca>
2019-04-13 02:25:54 -0400

    Make use of latest spring version.
    -Addressed review comments.
    
    Change-Id: Id4d3f48b22383c7b22e2931ade26192256def8aa
    Issue-ID: CCSDK-349
    Signed-off-by: prathamesh morde <prathamesh.morde@bell.ca>

--- ms/cds-sdc-listener/application/src/main/java/org/onap/ccsdk/cds/cdssdclistener/CdsSdcListenerApplication.java
+++ ms/cds-sdc-listener/application/src/main/java/org/onap/ccsdk/cds/cdssdclistener/CdsSdcListenerApplication.java
@@ -9,6 +9,7 @@
 package org.onap.ccsdk.cds.cdssdclistener;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
