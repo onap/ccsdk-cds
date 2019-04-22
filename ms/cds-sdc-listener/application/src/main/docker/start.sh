260fb3a3e080f4ec97e306fa1fb57cbace13584d
Oleg Mitsura <oleg.mitsura@amdocs.com>
2019-04-22 17:47:33 -0400

    1.Reworked Dockerization of SDC Listener to follow application/distribution+parent style; 2.Renamed CDS-SDClistener to just SDCListener
    Issue-ID: CCSDK-1145
    
    Change-Id: I007373d3a48fb8eb7042150bdbb37fc7fec346b3
    Signed-off-by: Oleg Mitsura <oleg.mitsura@amdocs.com>

--- ms/cds-sdc-listener/application/src/main/docker/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-extraArgs=$@
-java -jar /opt/app/onap/cds-sdc-listener-distribution.jar \
--Dspring.config=/opt/app/onap/config/application.yml \
--Djava.security.egd=file:/dev/./urandom \
-${extraArgs}
