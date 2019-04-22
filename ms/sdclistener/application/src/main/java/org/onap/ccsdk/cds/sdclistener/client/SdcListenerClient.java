f789906578311d4b661064ce00c32c93fc94a917
Oleg Mitsura <oleg.mitsura@amdocs.com>
2019-04-22 17:47:33 -0400

    SDC Listner Docker touchup
    1.Reworked Dockerization of SDC Listener to follow
     application/distribution+parent style;
    2.Renamed CDS-SDClistener to just SDCListener
    
    Issue-ID: CCSDK-1145
    
    Change-Id: I007373d3a48fb8eb7042150bdbb37fc7fec346b3
    Signed-off-by: Oleg Mitsura <oleg.mitsura@amdocs.com>

---
+++ ms/sdclistener/application/src/main/java/org/onap/ccsdk/cds/sdclistener/client/SdcListenerClient.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright © 2019 Bell Canada
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.onap.ccsdk.cds.sdclistener.client;
+
+import java.util.Optional;
+import org.onap.ccsdk.cds.sdclistener.SdcListenerConfiguration;
+import org.onap.ccsdk.cds.sdclistener.dto.SdcListenerDto;
+import org.onap.ccsdk.cds.sdclistener.SdcListenerNotificationCallback;
+import org.onap.ccsdk.cds.sdclistener.exceptions.SdcListenerException;
+import org.onap.sdc.api.IDistributionClient;
+import org.onap.sdc.api.results.IDistributionClientResult;
+import org.onap.sdc.impl.DistributionClientFactory;
+import org.onap.sdc.utils.DistributionActionResultEnum;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@ComponentScan("org.onap.ccsdk.cds.cdssdclistener.dto")
+public class SdcListenerClient {
+
+    private static Logger LOG = LoggerFactory.getLogger(SdcListenerClient.class);
+
+    @Autowired
+    private SdcListenerConfiguration configuration;
+
+    @Autowired
+    private SdcListenerNotificationCallback notification;
+
+    @Autowired
+    private SdcListenerDto listenerDto;
+
+    private IDistributionClient distributionClient;
+
+    /**
+     * This method initializes the SDC Distribution client.
+     */
+    @EventListener(ApplicationReadyEvent.class)
+    public void initSdcClient() throws SdcListenerException {
+        LOG.info("Initialize the SDC distribution client");
+
+        distributionClient = Optional.of(DistributionClientFactory.createDistributionClient())
+            .orElseThrow(() -> new SdcListenerException("Could not able to create SDC Distribution client"));
+
+        listenerDto.setManagedChannelForGrpc();
+
+        listenerDto.setDistributionClient(distributionClient);
+
+        IDistributionClientResult result = distributionClient.init(configuration, notification);
+        startSdcClientBasedOnTheResult(result);
+    }
+
+    private void startSdcClientBasedOnTheResult(IDistributionClientResult result) throws SdcListenerException {
+        if (!result.getDistributionActionResult().equals(DistributionActionResultEnum.SUCCESS)) {
+            throw new SdcListenerException(
+                "SDC distribution client init failed with reason:" + result.getDistributionMessageResult());
+        }
+
+        LOG.info("Initialization of the SDC distribution client is complete");
+
+        // Start the client.
+        result = this.distributionClient.start();
+
+        if (!result.getDistributionActionResult().equals(DistributionActionResultEnum.SUCCESS)) {
+            throw new SdcListenerException(
+                "Startup of the SDC distribution client failed with reason: " + result.getDistributionMessageResult());
+        }
+    }
+
+    private void closeSdcDistributionclient() throws SdcListenerException {
+        LOG.info("Closing SDC distribution client");
+        IDistributionClientResult status = this.distributionClient.stop();
+        if (status.getDistributionActionResult().equals(DistributionActionResultEnum.SUCCESS)) {
+            throw new SdcListenerException(
+                "Failed to close the SDC distribution client due to : " + status.getDistributionMessageResult());
+        }
+    }
+}
---
+++ ms/sdclistener/distribution/src/main/docker/startService.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+echo "Starting SDC Listener"
+
+echo "APP Config HOME : ${APP_CONFIG_HOME}"
+export APP_HOME=/opt/app/onap
+
+java -classpath "/etc:${APP_HOME}/lib/*:/lib/*:/src:/schema:/generated-sources:${APP_CONFIG_HOME}:${APP_HOME}" \
+-DappName=${APPLICATIONNAME} -DappVersion=${BUNDLEVERSION} \
+-Dms_name=org.onap.ccsdk.cds.sdclistener\
+-Dlogging.config=${APP_CONFIG_HOME}/logback.xml \
+-Djava.security.egd=file:/dev/./urandom \
+-DAPPNAME=${APP_NAME} -DAPPENV=${APP_ENV} -DAPPVERSION=${APP_VERSION} -DNAMESPACE=${NAMESPACE} \
+-Dspring.config.location=${APP_CONFIG_HOME}/ \
+org.onap.ccsdk.cds.sdclistener.SdcListenerApplication
