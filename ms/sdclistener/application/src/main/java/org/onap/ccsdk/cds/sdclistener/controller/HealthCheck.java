b26e4bdd161116a061a93bd6c48fa49743eacb4b
prathamesh morde <prathamesh.morde@bell.ca>
2019-05-03 10:10:02 -0400

    Change the incorrect license header.
    Change-Id: Ibbe2d363051c64e2f557c5ae4bb3164fe3087d3d
    Issue-ID: CCSDK-1304
    Signed-off-by: prathamesh morde <prathamesh.morde@bell.ca>
    (cherry picked from commit febb517be84666a67cb04a58f89aac4af788ac7c)

--- ms/sdclistener/application/src/main/java/org/onap/ccsdk/cds/sdclistener/controller/HealthCheck.java
+++ ms/sdclistener/application/src/main/java/org/onap/ccsdk/cds/sdclistener/controller/HealthCheck.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.onap.ccsdk.cds.sdclistener.controller;
 
 import org.springframework.http.MediaType;
