6635ab45b250c2df9c8662fb71b9ffae104635f4
prathamesh morde <prathamesh.morde@bell.ca>
2019-05-13 11:53:08 -0400

    Upates in SDC-LISTENER NOTIFICATION Code.
    -Make use of IComponentDoneStatusMessage instead of IFinalDistrStatusMessage.
    -Added DownloadStausMessage to handle download CSAR notification.
    
    Change-Id: Ie6bb0569e6daffb13e06532f670aac27af1f935e
    Issus-ID:CCSDK-1319
    Signed-off-by: prathamesh morde <prathamesh.morde@bell.ca>

---
+++ ms/sdclistener/application/src/main/java/org/onap/ccsdk/cds/sdclistener/status/DownloadStatusMessage.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright © 2019 Bell Canada
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.onap.ccsdk.cds.sdclistener.status;
+
+import org.onap.sdc.api.consumer.IDistributionStatusMessage;
+import org.onap.sdc.api.consumer.IDistributionStatusMessageBasic;
+import org.onap.sdc.utils.DistributionStatusEnum;
+//
+//public class DownloadStatusMessage implements IDistributionStatusMessage {
+//
+//    @Override
+//    public String getArtifactURL() {
+//        return null;
+//    }
+//
+//    @Override
+//    public String getDistributionID() {
+//        return null;
+//    }
+//
+//    @Override
+//    public String getConsumerID() {
+//        return null;
+//    }
+//
+//    @Override
+//    public long getTimestamp() {
+//        return 0;
+//    }
+//
+//    @Override
+//    public DistributionStatusEnum getStatus() {
+//        return null;
+//    }
+//}
