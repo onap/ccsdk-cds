a82e2dc612c2078e20b40efc7dbe5f5b6da80478
Nirvan Ramjuttun <nirvan.ramjuttun@amdocs.com>
2019-05-30 14:07:23 -0400

    CDS-UI: support gRPC integration with blueprint processor mS - For deploy blueprint functionality
    Change-Id: I35395ae315ac063c3bb3f6893956965ecb74e74e
    Issue-ID: CCSDK-1274
    Signed-off-by: Nirvan Ramjuttun <nirvan.ramjuttun@amdocs.com>

---
+++ cds-ui/server/src/config/app-config.ts
@@ -0,0 +1,32 @@
+export const appConfig = Object.freeze({
+    action: Object.freeze({
+        deployBlueprint: Object.freeze({
+            grpcEnabled: process.env.APP_ACTION_DEPLOY_BLUEPRINT_GRPC_ENABLED || true
+        })
+    })
+});
+
+export const controllerApiConfig = Object.freeze({
+    http: Object.freeze({
+        url: process.env.API_BLUEPRINT_CONTROLLER_HTTP_BASE_URL || "http://localhost:8080/api/v1",
+        authToken: process.env.API_BLUEPRINT_CONTROLLER_HTTP_AUTH_TOKEN || "Basic Y2NzZGthcHBzOmNjc2RrYXBwcw=="
+    })
+});
+
+export const processorApiConfig = Object.freeze({
+    http: Object.freeze({
+        url: process.env.API_BLUEPRINT_PROCESSOR_HTTP_BASE_URL || "http://localhost:8081/api/v1",
+        authToken: process.env.API_BLUEPRINT_PROCESSOR_HTTP_AUTH_TOKEN || "Basic Y2NzZGthcHBzOmNjc2RrYXBwcw=="
+    }),
+    grpc: Object.freeze({
+        host: process.env.API_BLUEPRINT_PROCESSOR_GRPC_HOST || "localhost",
+        port: process.env.API_BLUEPRINT_PROCESSOR_GRPC_PORT || 9111,
+        authToken: process.env.API_BLUEPRINT_PROCESSOR_GRPC_AUTH_TOKEN || "Basic Y2NzZGthcHBzOmNjc2RrYXBwcw==",
+        bluePrintManagement: Object.freeze({
+            //this path is relative to 'dist' folder
+            protoPath: __dirname + '../../../proto/BluePrintManagement.proto'
+        })
+    })
+});
+
+
