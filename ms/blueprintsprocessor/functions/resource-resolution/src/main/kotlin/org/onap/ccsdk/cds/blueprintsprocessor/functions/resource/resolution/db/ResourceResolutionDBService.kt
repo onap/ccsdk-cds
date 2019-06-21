ea4d7a1ea00eb295c95f8fb33ae3960cbd821dce
Alexis de Talhouët <adetalhouet89@gmail.com>
2019-06-20 22:48:12 -0400

    Refactor resolution controllers
    tempalte and resources for get/put API
    
    Change-Id: Ia00bdb08f5f52231481edc6de232f850c66874e9
    Issue-ID: CCSDK-1423
    Signed-off-by: Alexis de Talhouët <adetalhouet89@gmail.com>

--- ms/blueprintsprocessor/functions/resource-resolution/src/main/kotlin/org/onap/ccsdk/cds/blueprintsprocessor/functions/resource/resolution/db/ResourceResolutionDBService.kt
+++ ms/blueprintsprocessor/functions/resource-resolution/src/main/kotlin/org/onap/ccsdk/cds/blueprintsprocessor/functions/resource/resolution/db/ResourceResolutionDBService.kt
@@ -1,6 +1,5 @@
 /*
  * Copyright (C) 2019 Bell Canada.
- * Modifications Copyright © 2019 IBM.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -72,7 +71,7 @@ class ResourceResolutionDBService(private val resourceResolutionRepository: Reso
     suspend fun write(properties: Map<String, Any>,
                       bluePrintRuntimeService: BluePrintRuntimeService<*>,
                       artifactPrefix: String,
-                      resourceAssignment: ResourceAssignment) = withContext(Dispatchers.IO) {
+                      resourceAssignment: ResourceAssignment): ResourceResolution = withContext(Dispatchers.IO) {
 
         val metadata = bluePrintRuntimeService.bluePrintContext().metadata!!
 
