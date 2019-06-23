032c0895200250679fd3d9e0ff239c4329862a4f
Alexis de Talhouët <adetalhouet89@gmail.com>
2019-06-23 15:30:36 -0400

    Fix swagger definition for blueprint processor
    Change-Id: I69ba541b4b301735a988cf01673827a79daf1f8c
    Issue-ID: CCSDK-1431
    Signed-off-by: Alexis de Talhouët <adetalhouet89@gmail.com>

--- ms/blueprintsprocessor/modules/inbounds/resource-api/src/main/kotlin/org/onap/ccsdk/cds/blueprintsprocessor/resource/api/TemplateController.kt
+++ ms/blueprintsprocessor/modules/inbounds/resource-api/src/main/kotlin/org/onap/ccsdk/cds/blueprintsprocessor/resource/api/TemplateController.kt
@@ -16,10 +16,14 @@
 
 package org.onap.ccsdk.cds.blueprintsprocessor.resource.api
 
+import com.fasterxml.jackson.databind.JsonNode
+import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
+import io.swagger.annotations.ApiParam
 import kotlinx.coroutines.runBlocking
 import org.onap.ccsdk.cds.blueprintsprocessor.functions.resource.resolution.db.TemplateResolution
 import org.onap.ccsdk.cds.blueprintsprocessor.functions.resource.resolution.db.TemplateResolutionService
+import org.onap.ccsdk.cds.controllerblueprints.core.utils.JacksonUtils
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
@@ -33,26 +37,41 @@ import org.springframework.web.bind.annotation.*
  */
 @RestController
 @RequestMapping("/api/v1/template")
+@Api(value = "/api/v1/template",
+    description = "Interaction with resolved template.")
 open class TemplateController(private val templateResolutionService: TemplateResolutionService) {
 
-    @RequestMapping(path = ["/ping"], method = [RequestMethod.GET], produces = [MediaType.APPLICATION_JSON_VALUE])
+    @RequestMapping(path = ["/health-check"],
+        method = [RequestMethod.GET],
+        produces = [MediaType.APPLICATION_JSON_VALUE])
     @ResponseBody
-    fun ping(): String = runBlocking {
-        "Success"
+    @ApiOperation(value = "Health Check", hidden = true)
+    fun templateControllerHealthCheck(): JsonNode = runBlocking {
+        JacksonUtils.getJsonNode("Success")
     }
 
-    @RequestMapping(path = [""], method = [RequestMethod.GET], produces = [MediaType.TEXT_PLAIN_VALUE])
-    @ApiOperation(value = "Retrieve a meshed template.",
-        notes = "Retrieve a meshed template for a given CBA's action, identified by its blueprint name, blueprint version, " +
-                "artifact name and resolution key. And extra 'format' parameter can be passed to tell what content-type" +
+    @RequestMapping(path = [""],
+        method = [RequestMethod.GET],
+        produces = [MediaType.TEXT_PLAIN_VALUE, MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE])
+    @ApiOperation(value = "Retrieve a resolved template.",
+        notes = "Retrieve a config template for a given CBA's action, identified by its blueprint name, blueprint version, " +
+                "artifact name and resolution key. An extra 'format' parameter can be passed to tell what content-type" +
                 " to expect in return")
     @ResponseBody
     @PreAuthorize("hasRole('USER')")
-    fun get(@RequestParam(value = "bpName") bpName: String,
-            @RequestParam(value = "bpVersion") bpVersion: String,
-            @RequestParam(value = "artifactName") artifactName: String,
-            @RequestParam(value = "resolutionKey") resolutionKey: String,
-            @RequestParam(value = "format", required = false, defaultValue = "text/plain") format: String)
+    fun get(
+        @ApiParam(value = "Name of the CBA.", required = true)
+        @RequestParam(value = "bpName") bpName: String,
+        @ApiParam(value = "Version of the CBA.", required = true)
+        @RequestParam(value = "bpVersion") bpVersion: String,
+        @ApiParam(value = "Artifact name for which to retrieve a resolved resource.", required = true)
+        @RequestParam(value = "artifactName") artifactName: String,
+        @ApiParam(value = "Resolution Key associated with the resolution.", required = true)
+        @RequestParam(value = "resolutionKey") resolutionKey: String,
+        @ApiParam(value = "Expected format of the template being retrieved.",
+            defaultValue = MediaType.TEXT_PLAIN_VALUE,
+            required = true)
+        @RequestParam(value = "format", required = false, defaultValue = MediaType.TEXT_PLAIN_VALUE) format: String)
             : ResponseEntity<String> = runBlocking {
 
         val result = templateResolutionService.read(bpName, bpVersion, artifactName, resolutionKey)
@@ -68,15 +87,22 @@ open class TemplateController(private val templateResolutionService: TemplateRes
 
 
     @PostMapping("/{bpName}/{bpVersion}/{artifactName}/{resolutionKey}", produces = [MediaType.APPLICATION_JSON_VALUE])
-    @ApiOperation(value = "Store a meshed template",
-        notes = "Store a meshed template for a given CBA's action, identified by its blueprint name, blueprint version, " +
-                "artifact name and resolution key.")
+    @ApiOperation(value = "Store a resolved template",
+        notes = "Store a template for a given CBA's action, identified by its blueprint name, blueprint version, " +
+                "artifact name and resolution key.",
+        response = TemplateResolution::class,
+        produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     @PreAuthorize("hasRole('USER')")
-    fun post(@PathVariable(value = "bpName") bpName: String,
+    fun post(@ApiParam(value = "Name of the CBA.", required = true)
+             @PathVariable(value = "bpName") bpName: String,
+             @ApiParam(value = "Version of the CBA.", required = true)
              @PathVariable(value = "bpVersion") bpVersion: String,
+             @ApiParam(value = "Artifact name for which to retrieve a resolved resource.", required = true)
              @PathVariable(value = "artifactName") artifactName: String,
+             @ApiParam(value = "Resolution Key associated with the resolution.", required = true)
              @PathVariable(value = "resolutionKey") resolutionKey: String,
+             @ApiParam(value = "Template to store.", required = true)
              @RequestBody result: String): ResponseEntity<TemplateResolution> = runBlocking {
 
         val resultStored =
