a6cd61a7b744db4e7e6a6b1bdba6d0b5cb2660e2
Brinda Santh <brindasanth@in.ibm.com>
2019-07-18 16:59:19 -0400

    Refactor Netconf script component parent.
    Change-Id: Ibbec8cd5785372a89e14a86d4e6ff7f9fed4aad2
    Issue-ID: CCSDK-1499
    Signed-off-by: Brinda Santh <brindasanth@in.ibm.com>
    Signed-off-by: Steve Siani <alphonse.steve.siani.djissitchi@ibm.com>

--- ms/blueprintsprocessor/functions/restconf-executor/src/main/kotlin/org/onap/ccsdk/cds/blueprintsprocessor/functions/restconf/executor/RestconfExecutorExtensions.kt
+++ ms/blueprintsprocessor/functions/restconf-executor/src/main/kotlin/org/onap/ccsdk/cds/blueprintsprocessor/functions/restconf/executor/RestconfExecutorExtensions.kt
@@ -1,5 +1,6 @@
 /*
  *  Copyright © 2019 IBM.
+ *  Modifications Copyright © 2018-2019 IBM, Bell Canada
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -14,9 +15,23 @@
  *  limitations under the License.
  */
 
-package org.onap.ccsdk.cds.blueprintsprocessor.functions.cli.executor
+package org.onap.ccsdk.cds.blueprintsprocessor.functions.restconf.executor
 
 /**
- * Register the CLI module exposed dependency
+ * Register the Restconf module exposed dependency
  */
 
+
+/**
+ * Generic Mount function
+ */
+
+
+/**
+ * Generic Configure function
+ */
+
+
+/**
+ * Generic Unmount function
+ */
