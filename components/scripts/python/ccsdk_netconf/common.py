505bf83b76eb80beca189c78bcbc0195b598eed6
Eliezio Oliveira <eliezio.oliveira@est.tech>
2019-07-23 09:38:16 +0100

    Fix Non-ASCII character on module ccsdk_netconf/common.py
    Change-Id: Id21362059f7fb3cfb6756f12adfc60d051764b5e
    Issue-ID: CCSDK-1517
    Signed-off-by: Eliezio Oliveira <eliezio.oliveira@est.tech>

--- components/scripts/python/ccsdk_netconf/common.py
+++ components/scripts/python/ccsdk_netconf/common.py
@@ -1,5 +1,5 @@
 #  Copyright (c) 2019 Bell Canada.
-#  Modifications Copyright © 2018 - 2019 IBM, Bell Canada.
+#  Modifications Copyright (c) 2018 - 2019 IBM, Bell Canada.
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
