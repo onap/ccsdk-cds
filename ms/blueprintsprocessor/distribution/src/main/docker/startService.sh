dd45e7c651b95a0224fad37022d838ab0f18265e
Abdelmuhaimen Seaudi <abdelmuhaimen.seaudi@orange.com>
2019-07-25 19:18:15 +0000

    Import ONAP root CA to CDS Blueprint Processor Java Truststore
    Issue-ID: CCSDK-1534
    Signed-off-by: Abdelmuhaimen Seaudi <abdelmuhaimen.seaudi@orange.com>
    Change-Id: Id5de634af72fc4a5962867f279d51fa35de25619

--- ms/blueprintsprocessor/distribution/src/main/docker/startService.sh
+++ ms/blueprintsprocessor/distribution/src/main/docker/startService.sh
@@ -5,4 +5,6 @@ nodeName=BlueprintsProcessor_1.0.0_$(cat /proc/self/cgroup | grep docker | sed s
 echo "APP Config HOME : ${APP_CONFIG_HOME}"
 export APP_HOME=/opt/app/onap
 
+keytool -import -trustcacerts -keystore $JAVA_HOME/jre/lib/security/cacerts -storepass changeit -alias AAF -import -file $APP_CONFIG_HOME/AAF_RootCA.cer
+
 source /etc/run.source
