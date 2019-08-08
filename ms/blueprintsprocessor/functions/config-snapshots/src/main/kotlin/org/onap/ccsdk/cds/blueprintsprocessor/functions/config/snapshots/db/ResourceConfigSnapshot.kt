34c424689a52614fb414d65899282497fe25b164
Serge Simard <serge@agilitae.com>
2019-08-08 10:55:57 -0400

    Resource Configuration Snapshots Executor and API
    Issue-ID: CCSDK-1604
    Signed-off-by: Serge Simard <serge@agilitae.com>
    Change-Id: I349c649e941431b48a309123489d26fb22e0e50a
    Signed-off-by: Serge Simard <serge@agilitae.com>

---
+++ ms/blueprintsprocessor/functions/config-snapshots/src/main/kotlin/org/onap/ccsdk/cds/blueprintsprocessor/functions/config/snapshots/db/ResourceConfigSnapshot.kt
@@ -0,0 +1,85 @@
+/*
+ * Copyright © 2019 Bell Canada
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.onap.ccsdk.cds.blueprintsprocessor.functions.config.snapshots.db
+
+import com.fasterxml.jackson.annotation.JsonFormat
+import io.swagger.annotations.ApiModelProperty
+import org.hibernate.annotations.Proxy
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.io.Serializable
+import java.util.*
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.EntityListeners
+import javax.persistence.Id
+import javax.persistence.Lob
+import javax.persistence.Table
+import javax.persistence.Temporal
+import javax.persistence.TemporalType
+
+/**
+ * ResourceConfigSnapshot model
+ * Stores RUNNING or CANDIDATE resource configuration snapshots, captured during the execution
+ * of blueprints.  A resource is identified by an identifier and a type.
+ *
+ * @author Serge Simard
+ * @version 1.0
+ */
+@EntityListeners(AuditingEntityListener::class)
+@Entity
+@Table(name = "RESOURCE_CONFIG_SNAPSHOT")
+@Proxy(lazy = false)
+class ResourceConfigSnapshot : Serializable {
+
+    @get:ApiModelProperty(value = "Resource type.", required = true, example = "ServiceInstance, VfModule, VNF, PNF")
+    @Column(name = "resource_type", nullable = false)
+    var resourceType: String? = null
+
+    @get:ApiModelProperty(value = "ID associated with the resource type in the inventory system.", required = true)
+    @Column(name = "resource_id", nullable = false)
+    var resourceId: String? = null
+
+    @get:ApiModelProperty(value = "Status of the snapshot, either running or candidate.", required = true)
+    @Column(name = "status", nullable = false)
+    var status: Status? = null
+
+    @get:ApiModelProperty(value = "Snapshot of the resource as retrieved from resource.", required = true)
+    @Lob
+    @Column(name = "config_snapshot", nullable = false)
+    var config_snapshot: String? = null
+
+    @Id
+    @Column(name = "resource_config_snapshot_id")
+    var id: String? = null
+
+    @get:ApiModelProperty(value = "Creation date of the snapshot.", required = true)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+    @LastModifiedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "creation_date")
+    var createdDate = Date()
+
+    companion object {
+        private const val serialVersionUID = 1L
+    }
+
+    enum class Status(val state: String) {
+        RUNNING("RUNNING"),
+        CANDIDATE("CANDIDATE")
+    }
+}
---
+++ ms/blueprintsprocessor/modules/inbounds/configs-api/src/test/kotlin/org/onap/ccsdk/cds/blueprintsprocessor/configs/api/ResourceConfigSnapshotControllerTest.kt
@@ -0,0 +1,175 @@
+/*
+ * Copyright © 2019 Bell Canada.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.onap.ccsdk.cds.blueprintsprocessor.configs.api
+
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.onap.ccsdk.cds.blueprintsprocessor.core.BluePrintCoreConfiguration
+import org.onap.ccsdk.cds.controllerblueprints.core.interfaces.BluePrintCatalogService
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.security.SecurityProperties
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.http.MediaType
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.TestPropertySource
+import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.web.reactive.function.BodyInserters
+import java.util.*
+
+@RunWith(SpringRunner::class)
+@WebFluxTest
+@ContextConfiguration(classes = [BluePrintCoreConfiguration::class,
+    BluePrintCatalogService::class, SecurityProperties::class])
+@ComponentScan(basePackages = ["org.onap.ccsdk.cds.blueprintsprocessor", "org.onap.ccsdk.cds.controllerblueprints"])
+@TestPropertySource(locations = ["classpath:application-test.properties"])
+class ResourceConfigSnapshotControllerTest {
+
+    private val log = LoggerFactory.getLogger(ResourceConfigSnapshotControllerTest::class.toString())
+
+    @Autowired
+    lateinit var webTestClient: WebTestClient
+
+    val resourceId = "fcaa6ac3ff08"
+    val resourceType = "PNF"
+    val snapshotData = "PAYLOAD DATA"
+
+    var requestArguments = "resourceId=$resourceId&resourceType=$resourceType"
+
+    @Test
+    fun `ping return Success`() {
+        runBlocking {
+            webTestClient.get().uri("/api/v1/configs/health-check")
+                .exchange()
+                .expectStatus().isOk
+                .expectBody()
+                .equals("Success")
+        }
+    }
+
+    @Test
+    fun `update configuration is allowed and updates timestamp`() {
+        runBlocking {
+
+            webTestClient
+                    .post()
+                    .uri("/api/v1/configs/$resourceType/$resourceId/running")
+                    .body(BodyInserters.fromObject(snapshotData))
+                    .exchange()
+                    .expectStatus().is2xxSuccessful
+                    .expectBody()
+                    .jsonPath("$.createdDate")
+                        .value<String> { println(it) }
+
+            webTestClient
+                    .post()
+                    .uri("/api/v1/configs/$resourceType/$resourceId/running")
+                    .body(BodyInserters.fromObject(snapshotData))
+                    .exchange()
+                    .expectStatus().is2xxSuccessful
+                    .expectBody()
+                    .jsonPath("$.createdDate")
+                        .value<String> { println(it)}
+        }
+    }
+
+    @Test
+    fun `get returns requested JSON content-type`() {
+        runBlocking {
+            post(resourceType, "22", "RUNNING")
+            get("json", resourceType,"22", "RUNNING")
+        }
+    }
+
+    @Test
+    fun `get returns requested XML content-type`() {
+        runBlocking {
+            post(resourceType, "3", "CANDIDATE")
+            get("xml", resourceType, "3", "CANDIDATE")
+        }
+    }
+
+    @Test
+    fun `get returns 400 error if missing arg`() {
+        runBlocking {
+            val arguments = "artifactName=WRONGARG1&resolutionKey=WRONGARG1"
+
+            webTestClient.get().uri("/api/v1/configs?$arguments")
+                .exchange()
+                .expectStatus().isBadRequest
+        }
+    }
+
+    @Test
+    fun `get returns 400 error if wrong Status arg`() {
+        runBlocking {
+            val arguments = "resourceId=MISSING&resourceType=PNF&status=TOTALLY_WRONG"
+
+            webTestClient.get().uri("/api/v1/configs?$arguments")
+                    .exchange()
+                    .expectStatus().isBadRequest
+        }
+    }
+
+    @Test
+    fun `get returns 404 if entry not found`() {
+        runBlocking {
+
+            webTestClient
+                .get()
+                .uri("/api/v1/configs?resourceId=MISSING&resourceType=PNF")
+                .exchange()
+                .expectStatus().isNotFound
+        }
+    }
+
+    private fun post( resourceType: String, resourceId: String, status: String) {
+        webTestClient
+            .post()
+            .uri("/api/v1/configs/$resourceType/$resourceId/$status")
+            .body(BodyInserters.fromObject(snapshotData))
+            .exchange()
+            .expectStatus().is2xxSuccessful
+            .expectBody()
+    }
+
+    private fun get(expectedType : String, resourceType: String, resourceId: String, status: String) {
+        var requestArguments = "resourceId=$resourceId&resourceType=$resourceType&status=$status"
+
+        if (expectedType.isNotEmpty()) {
+            requestArguments = "$requestArguments&format=$expectedType"
+            webTestClient
+                .get()
+                .uri("/api/v1/configs?$requestArguments")
+                .exchange()
+                .expectStatus().is2xxSuccessful
+                .expectHeader().contentType(MediaType.valueOf("application/$expectedType"))
+                .expectBody().equals(snapshotData)
+        } else {
+            webTestClient
+                .get()
+                .uri("/api/v1/configs?$requestArguments")
+                .exchange()
+                .expectStatus().is2xxSuccessful
+                .expectHeader().contentType(MediaType.TEXT_PLAIN)
+                .expectBody().equals(snapshotData)
+        }
+    }
+}
