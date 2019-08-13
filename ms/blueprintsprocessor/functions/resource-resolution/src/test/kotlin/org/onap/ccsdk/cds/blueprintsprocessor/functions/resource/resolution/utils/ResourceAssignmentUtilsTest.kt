c2964cac56fe151b5705dbd2f388a4f0e518187a
Steve Siani <alphonse.steve.siani.djissitchi@ibm.com>
2019-08-13 16:46:21 -0400

    Unit tests to cover all use-cases of complex type of DD and Data Type
    Issue-ID: CCSDK-1615
    Signed-off-by: Steve Siani <alphonse.steve.siani.djissitchi@ibm.com>
    Change-Id: I1035aa05b4b038095c9d17fdcd00c462bed5085a

--- ms/blueprintsprocessor/functions/resource-resolution/src/test/kotlin/org/onap/ccsdk/cds/blueprintsprocessor/functions/resource/resolution/utils/ResourceAssignmentUtilsTest.kt
+++ ms/blueprintsprocessor/functions/resource-resolution/src/test/kotlin/org/onap/ccsdk/cds/blueprintsprocessor/functions/resource/resolution/utils/ResourceAssignmentUtilsTest.kt
@@ -1,6 +1,7 @@
 /*-
  * ============LICENSE_START=======================================================
  *  Copyright (C) 2019 Nordix Foundation.
+ *  Modifications Copyright © 2019 IBM, Bell Canada
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,13 +22,58 @@
 
 package org.onap.ccsdk.cds.blueprintsprocessor.functions.resource.resolution.utils
 
+import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.node.TextNode
+import io.mockk.every
+
+import io.mockk.mockk
+import io.mockk.spyk
+import org.junit.Before
 import org.junit.Test
+import org.onap.ccsdk.cds.blueprintsprocessor.functions.resource.resolution.ResourceAssignmentRuntimeService
+import org.onap.ccsdk.cds.blueprintsprocessor.functions.resource.resolution.utils.ResourceAssignmentUtils.Companion.parseResponseNode
+import org.onap.ccsdk.cds.controllerblueprints.core.data.DataType
 import org.onap.ccsdk.cds.controllerblueprints.core.data.PropertyDefinition
+import org.onap.ccsdk.cds.controllerblueprints.core.service.BluePrintRuntimeService
+import org.onap.ccsdk.cds.controllerblueprints.core.utils.BluePrintMetadataUtils
+import org.onap.ccsdk.cds.controllerblueprints.core.utils.JacksonUtils
 import org.onap.ccsdk.cds.controllerblueprints.resource.dict.ResourceAssignment
 import kotlin.test.assertEquals
 
+data class IpAddresses(val name: String, val ip: String)
+
 class ResourceAssignmentUtilsTest {
+    private lateinit var resourceAssignmentRuntimeService: ResourceAssignmentRuntimeService
+
+    @Before
+    fun setup() {
+
+        val bluePrintContext = BluePrintMetadataUtils.getBluePrintContext(
+                "./../../../../components/model-catalog/blueprint-model/test-blueprint/baseconfiguration")
+
+        resourceAssignmentRuntimeService = spyk(ResourceAssignmentRuntimeService("1234", bluePrintContext))
+
+        val bluePrintRuntimeService = mockk<BluePrintRuntimeService<*>>()
+
+        val propertiesDefinition1 = PropertyDefinition().apply {
+            type = "string"
+            id = "name"
+        }
+
+        val propertiesDefinition2 = PropertyDefinition().apply {
+            type = "string"
+            id = "address"
+        }
+
+        val myDataType = DataType().apply {
+            id = "myDataType"
+            properties = mutableMapOf("property1" to propertiesDefinition1, "property2" to propertiesDefinition2)
+        }
+
+        every { resourceAssignmentRuntimeService.bluePrintContext().dataTypeByName("myDataType") } returns myDataType
+
+        every { resourceAssignmentRuntimeService.setNodeTemplateAttributeValue(any(), any(), any()) } returns Unit
+    }
 
     @Test
     fun `generateResourceDataForAssignments - positive test`() {
@@ -75,5 +121,114 @@ class ResourceAssignmentUtilsTest {
             }
         }
         return resourceAssignmentForTest
+    }
+
+    @Test
+    fun parseResponseNodeTest(){
+        // Input values for primitive type
+        val value = arrayListOf("1.2.3.1")
+        val keyValue = mutableMapOf<String, String>()
+        keyValue["value"]= "1.2.3.1"
+
+        // Input values for collection type
+        val arrayOfString = arrayListOf("1.2.3.1", "1.2.3.2", "1.2.3.3")
+        val arrayOfKeyValue = arrayListOf(IpAddresses("value", "1.2.3.1"), IpAddresses("value", "1.2.3.2"), IpAddresses("value", "1.2.3.3"))
+        val mapOfString = mutableMapOf<String, String>()
+        mapOfString["value1"] = "1.2.3.1"
+        mapOfString["value2"] = "1.2.3.2"
+        mapOfString["value3"] = "1.2.3.3"
+        val mapOfkeyValue = mutableMapOf<String, IpAddresses>()
+        mapOfkeyValue["data1"] = IpAddresses("value", "1.2.3.1")
+        mapOfkeyValue["data2"] = IpAddresses("value", "1.2.3.2")
+        mapOfkeyValue["data3"] = IpAddresses("value", "1.2.3.3")
+
+        var outcome: JsonNode
+
+        // Test primitives types
+        val expectedPrimitiveType = TextNode("1.2.3.1")
+        outcome = prepareResponsenodeForTest("sample-value", "string", "", value)
+        assertEquals(expectedPrimitiveType, outcome, "unexpected outcome returned for primitive type of simple String")
+        outcome = prepareResponsenodeForTest("sample-key-value", "string", "", keyValue)
+        assertEquals(expectedPrimitiveType, outcome, "unexpected outcome returned for primitive type of key-value String")
+
+        // Test collection types
+        val expectedCollectionString = mutableMapOf<String, JsonNode>()
+        /*expectedCollectionString["value1"]=
+        TextNode("1.2.3.1")
+        outcome = prepareResponsenodeForTest("sample-value", "string", "", value)
+        assertEquals(expectedPrimitiveType, outcome, "unexpected outcome returned for primitive type of simple String")
+        outcome = prepareResponsenodeForTest("sample-key-value", "string", "", keyValue)
+        assertEquals(expectedPrimitiveType, outcome, "unexpected outcome returned for primitive type of key-value String")*/
+
+    }
+
+    private fun prepareResponsenodeForTest(dictionary_source: String, sourceType: String, entrySchema: String,
+                                           response: Any): JsonNode {
+
+        val resourceAssignment = when (sourceType) {
+            "list", "map" -> {
+                prepareRADataDictionnaryCollection(dictionary_source, sourceType, entrySchema)
+            }
+            "string" -> {
+                prepareRADataDictionnaryOfPrimaryType(dictionary_source)
+            }
+            else -> {
+                prepareRADataDictionnaryCollection(dictionary_source, sourceType, entrySchema)
+            }
+        }
+
+        val responseNode = checkNotNull(JacksonUtils.getJsonNode(response)) {
+            "Failed to get database query result into Json node."
+        }
+
+        val outputKeyMapping = prepareOutputKeyMapping(dictionary_source)
+
+        return parseResponseNode(responseNode, resourceAssignment, resourceAssignmentRuntimeService, outputKeyMapping)
+    }
+
+    private fun prepareRADataDictionnaryCollection(dictionary_source: String, sourceType: String, entrySchema: String): ResourceAssignment {
+        return ResourceAssignment().apply {
+            name = "ipAddress-list"
+            dictionaryName = "sample-licenses"
+            dictionarySource = "$dictionary_source"
+            property = PropertyDefinition().apply {
+                type = "$sourceType"
+                entrySchema.apply {
+                    type = "$entrySchema"
+                }
+            }
+        }
+    }
+
+    private fun prepareRADataDictionnaryOfPrimaryType(dictionary_source: String): ResourceAssignment {
+        return ResourceAssignment().apply {
+            name = "ipAddress"
+            dictionaryName = "sample-ip"
+            dictionarySource = "$dictionary_source"
+            property = PropertyDefinition().apply {
+                type = "string"
+            }
+        }
+    }
+
+    private fun prepareOutputKeyMapping(dictionary_source: String): MutableMap<String, String> {
+        val outputMapping = mutableMapOf<String, String>()
+
+        when (dictionary_source) {
+            "listOfString", "mapOfString" -> {
+                outputMapping["value"] = "value"
+            }
+            "listOfMyDataType", "mapOfMyDataType" -> {
+                outputMapping["name"] = "name"
+                outputMapping["address"] = "address"
+            }
+            "sample-key-value", "sample-value" -> {
+                outputMapping["sample-ip"] = "value"
+            }
+            else -> {
+                outputMapping["value"] = "value"
+            }
+        }
+        return outputMapping
     }
 }
