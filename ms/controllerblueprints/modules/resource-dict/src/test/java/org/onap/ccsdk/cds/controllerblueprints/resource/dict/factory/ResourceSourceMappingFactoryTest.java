f41f08364117748f13342ad87cdab43f99f1009b
Oleg Mitsura <oleg.mitsura@amdocs.com>
2019-08-15 16:41:17 -0400

    Make source-db consistent across samples
    Issue-Id: CCSDK-1623
    
    Signed-off-by: Oleg Mitsura <oleg.mitsura@amdocs.com>
    Change-Id: Iec7c6909c39c34894be11bfdac45f6394010d9cf

--- ms/controllerblueprints/modules/resource-dict/src/test/java/org/onap/ccsdk/cds/controllerblueprints/resource/dict/factory/ResourceSourceMappingFactoryTest.java
+++ ms/controllerblueprints/modules/resource-dict/src/test/java/org/onap/ccsdk/cds/controllerblueprints/resource/dict/factory/ResourceSourceMappingFactoryTest.java
@@ -25,15 +25,14 @@ public class ResourceSourceMappingFactoryTest {
     @Test
     public void testRegisterResourceMapping() {
 
-        ResourceSourceMappingFactory.INSTANCE.registerSourceMapping("primary-db", "source-processor-db");
-        ResourceSourceMappingFactory.INSTANCE.registerSourceMapping("processor-db", "source-processor-db");
+        ResourceSourceMappingFactory.INSTANCE.registerSourceMapping("processor-db", "source-db");
         ResourceSourceMappingFactory.INSTANCE.registerSourceMapping("input", "source-input");
         ResourceSourceMappingFactory.INSTANCE.registerSourceMapping("default", "source-default");
-        ResourceSourceMappingFactory.INSTANCE.registerSourceMapping("primary-config-data", "source-rest");
-        ResourceSourceMappingFactory.INSTANCE.registerSourceMapping("primary-aai-data", "source-rest");
+        ResourceSourceMappingFactory.INSTANCE.registerSourceMapping("config-data", "source-rest");
+        ResourceSourceMappingFactory.INSTANCE.registerSourceMapping("aai-data", "source-rest");
 
-        String nodeTypeName = ResourceSourceMappingFactory.INSTANCE.getRegisterSourceMapping("primary-db");
-        Assert.notNull(nodeTypeName, "Failed to get primary-db mapping");
+        String nodeTypeName = ResourceSourceMappingFactory.INSTANCE.getRegisterSourceMapping("processor-db");
+        Assert.notNull(nodeTypeName, "Failed to get processor-db mapping");
 
         ResourceSourceMapping resourceSourceMapping = ResourceSourceMappingFactory.INSTANCE.getRegisterSourceMapping();
         Assert.notNull(resourceSourceMapping, "Failed to get resource source mapping");
