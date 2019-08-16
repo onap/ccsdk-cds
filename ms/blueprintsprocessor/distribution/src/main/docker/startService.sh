a323fb42a5bb33ae791eab7379e0f91d38fd72ea
Adam Czajkowski <a.czajkowski@samsung.com>
2019-08-16 10:31:02 +0200

    [cds] Added enviroment variable-passed password handling.
    Issue-ID: OOM-1933
    Signed-off-by: Adam Czajkowski <a.czajkowski@samsung.com>
    Change-Id: If15cbda88fbbba35eb6b07c6b6f922500de734e4

--- ms/blueprintsprocessor/distribution/src/main/docker/startService.sh
+++ ms/blueprintsprocessor/distribution/src/main/docker/startService.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 nodeName=BlueprintsProcessor_1.0.0_$(cat /proc/self/cgroup | grep docker | sed s/\\//\\n/g | tail -1)
 
@@ -6,5 +6,29 @@ echo "APP Config HOME : ${APP_CONFIG_HOME}"
 export APP_HOME=/opt/app/onap
 
 keytool -import -trustcacerts -keystore $JAVA_HOME/jre/lib/security/cacerts -storepass changeit -alias ONAP -import -file $APP_CONFIG_HOME/ONAP_RootCA.cer
+
+PASSWORD_OVERRIDES=""
+
+if ! [ -z "${DB_PASSWORD+x}" ]; then
+  PASSWORD_OVERRIDES="${PASSWORD_OVERRIDES} -Dblueprintsprocessor.db.primary.password=${DB_PASSWORD}"
+fi
+
+if ! [ -z "${USER_PASSWORD+x}" ]; then
+  PASSWORD_OVERRIDES="${PASSWORD_OVERRIDES} -Dsecurity.user.password=${USER_PASSWORD}"
+fi
+
+if ! [ -z "${SDNCODL_PASSWORD+x}" ]; then
+  PASSWORD_OVERRIDES="${PASSWORD_OVERRIDES} -Dblueprintsprocessor.restclient.sdncodl.password=${SDNCODL_PASSWORD}"
+fi
+
+if ! [ -z "${CONFIG_DATA_PASSWORD+x}" ]; then
+  PASSWORD_OVERRIDES="${PASSWORD_OVERRIDES} -Dblueprintsprocessor.restclient.primary-config-data.password=${CONFIG_DATA_PASSWORD}"
+fi
+
+if ! [ -z "${AAI_DATA_PASSWORD+x}" ]; then
+  PASSWORD_OVERRIDES="${PASSWORD_OVERRIDES} -Dblueprintsprocessor.restclient.primary-aai-data.password=${AAI_DATA_PASSWORD}"
+fi
+
+echo "PASSWORD_OVERRIDES: ${PASSWORD_OVERRIDES:-NONE}"
 
 source /etc/run.source
--- ms/controllerblueprints/distribution/src/main/docker/startService.sh
+++ ms/controllerblueprints/distribution/src/main/docker/startService.sh
@@ -1,8 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
 nodeName=ControllerBlueprints_1.0.0_$(cat /proc/self/cgroup | grep docker | sed s/\\//\\n/g | tail -1)
 
 echo "APP Config HOME : ${APP_CONFIG_HOME}"
 export APP_HOME=/opt/app/onap
+
+PASSWORD_OVERRIDES=""
+
+if ! [ -z "${AUTH_HASHED_PASSWORD+x}" ]; then
+  PASSWORD_OVERRIDES="${PASSWORD_OVERRIDES} -Dbasic-auth.hashed-pwd=${AUTH_HASHED_PASSWORD}"
+fi
+
+if ! [ -z "${SPRING_DATASOURCE_PASSWORD+x}" ]; then
+  PASSWORD_OVERRIDES="${PASSWORD_OVERRIDES} -Dspring.datasource.password=${SPRING_DATASOURCE_PASSWORD}"
+fi
 
 source /etc/run.source
