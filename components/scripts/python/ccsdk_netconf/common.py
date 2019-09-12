eed244a5964b26d35bb8db3acb19311d781f8a58
Steve Siani <alphonse.steve.siani.djissitchi@ibm.com>
2019-09-12 15:38:23 -0400

    Add wrapper function for Cli and Netconf executor
    Issue-ID: CCSDK-1707
    Signed-off-by: Steve Siani <alphonse.steve.siani.djissitchi@ibm.com>
    Change-Id: I2c29d0135b8252cd21ed3d139516ac637c846927
    (cherry picked from commit 1d7e5f7f80281823cbfc4a2e2ea9833c6412de8c)

--- components/scripts/python/ccsdk_netconf/common.py
+++ components/scripts/python/ccsdk_netconf/common.py
@@ -12,6 +12,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+from netconf_constant import *
 from org.onap.ccsdk.cds.blueprintsprocessor.functions.resource.resolution import ResourceResolutionExtensionsKt
 
 
@@ -21,9 +22,45 @@ class ResolutionHelper:
         self.component_function = component_function
 
     def resolve_and_generate_message_from_template_prefix(self, artifact_prefix):
+        """Get the template resolved in the current workflow execution by the artifact prefix
+
+        :param artifact_prefix:
+        :return: template
+        """
         return ResourceResolutionExtensionsKt.contentFromResolvedArtifact(self.component_function, artifact_prefix)
 
     def retrieve_resolved_template_from_database(self, key, artifact_template):
+        """Get the template resolved and stored with resolution-key and matching with the artifact name
+
+        :param key:
+        :param artifact_template:
+        :return:
+        """
         return ResourceResolutionExtensionsKt.storedContentFromResolvedArtifact(self.component_function, key,
                                                                                 artifact_template)
+
+    def set_execution_attribute_response_data(self, response_data):
+        """For the current node execution, set the attribute value of response-data
+
+        :param response_data:
+        :return:
+        """
+        self.component_function.setAttribute(ATTRIBUTE_RESPONSE_DATA, response_data)
+
+    def get_node_template_attribute(self, node_template_name, attribute_key):
+        """get attribute value for a specific node template of the current workflow
+
+        :param node_template_name:
+        :param attribute_key:
+        :return: JsonNode
+        """
+        self.component_function.getNodeTemplateAttribute(node_template_name, attribute_key)
+
+    def get_input_value(self, key):
+        """Get input value of the current node template execution
+
+        :param key:
+        :return: JsonNode
+        """
+        self.component_function.bluePrintRuntimeService.getInputValue(key)
 
