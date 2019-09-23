807fbaae96dc75294fe17c933b43261011f33309
Brinda Santh <bs2796@att.com>
2019-09-23 10:10:58 -0400

    Add bi-directional GRPC python executor.
    Python based bidirectional communication script executor microservice.
    
    Issue-ID: CCSDK-1747
    Signed-off-by: Brinda Santh <bs2796@att.com>
    Change-Id: If2e8f862ca4cb97d10714df2ab1cccccef2d576a

---
+++ ms/py-executor/blueprints_grpc/script_executor_configuration.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+#
+#  Copyright © 2018-2019 AT&T Intellectual Property.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import os
+import logging
+import configparser
+from pathlib import Path, PurePath
+
+
+class ScriptExecutorConfiguration:
+
+    def __init__(self, file_path: str):
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.logger.info('loading configuration file : {}'.format(file_path))
+        self.config = configparser.ConfigParser(os.environ)
+        self.config.read(file_path, encoding='utf-8')
+
+    def get_section(self, section_name: str):
+        return self.config[section_name]
+
+    def get_property(self, section_name: str, property_name: str):
+        return self.config.get(section_name, property_name)
+
+    def script_executor_property(self, property_name: str):
+        return self.config.get('scriptExecutor', property_name)
+
+    def blueprints_processor(self, property_name: str):
+        return self.config.get('blueprintsprocessor', property_name)
+
+
+if __name__ == '__main__':
+    config_file = str(PurePath(Path().absolute())) + '/../configuration.ini'
+    scriptExecutorConfiguration = ScriptExecutorConfiguration(config_file)
+    blueprintDeployPath = scriptExecutorConfiguration.get_property('blueprintsprocessor', 'blueprintDeployPath')
+    print(blueprintDeployPath)
---
+++ ms/py-executor/docker/start.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+#
+# Copyright (C) 2019 Bell Canada.
+# Modifications Copyright © 2018-2019 AT&T Intellectual Property.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+if [ -z "${APP_PORT}" ]
+then
+  echo "APP_PORT environment variable is not set, using default."
+  export APP_PORT=50052
+fi
+
+if [ -z "${BASIC_AUTH}" ]
+then
+  echo "BASIC_AUTH environment variable is not set, using default."
+  export BASIC_AUTH="Basic Y2NzZGthcHBzOmNjc2RrYXBwcw=="
+fi
+
+if [ -z "${LOG_FILE}" ]
+then
+  echo "LOG_FILE environment variable is not set, using default."
+  export LOG_FILE="application.log"
+fi
+
+if [ "${http_proxy}" ]
+then
+  echo "Setting http_proxy: ${http_proxy}"
+fi
+
+if [ "${https_proxy}" ]
+then
+  echo "Setting https_proxy: ${https_proxy}"
+fi
+
+cd /opt/app/onap/python/
+python server.py
