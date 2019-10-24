14349209fbec4d54f25f502a40b6f650a63c8483
Serge Simard <serge@agilitae.com>
2019-10-24 01:42:27 -0400

    Improve Remote Python Executor error handling and
    Issue-ID: CCSDK-1855
    
    Signed-off-by: Serge Simard <serge@agilitae.com>
    Change-Id: I924918fb06a5a0f118772dedaa41c536c7eca9e7
    Signed-off-by: Serge Simard <serge@agilitae.com>
    (cherry picked from commit 35ab81bb9ff203e14116de10bc2735ffb2883f67)

--- components/model-catalog/blueprint-model/test-blueprint/remote_scripts/Scripts/python/SamplePython.py
+++ components/model-catalog/blueprint-model/test-blueprint/remote_scripts/Scripts/python/SamplePython.py
@@ -2,4 +2,14 @@
 
 import sys
 
+#Optional : import this utility class if returning payload
+from cds_utils.payload_coder import send_response_data_payload
+
+# Do your work...  using try .. except to handle errors and return False if error
 print(sys.argv[1])
+
+# Optional : return a JSON payload to the be published under the response-data output attribute
+send_response_data_payload({"étudiant": ["Mélanie", "Joséphine"]})
+
+# Always return a boolean indicating success or not..
+sys.exit(True)
