843d1f7ec7c45e3aad1d5198b776bb99a235448d
Shaaban Ebrahim <shaaban.eltanany.ext@orange.com>
2019-10-24 12:37:21 +0200

    Update on Health-api
    -add Combined health check
    -add combined metrics check
    
    Issue-ID: CCSDK-1669
    Signed-off-by: Shaaban Ebrahim <shaaban.eltanany.ext@orange.com>
    Change-Id: Idb3c7f67b3f22bd6069f75c193ae458c346fb2ac

---
+++ ms/blueprintsprocessor/modules/inbounds/health-api-common/src/main/kotlin/org/onap/ccsdk/cds/blueprintsprocessor/healthapi/configuration/SecurityEncryptionConfiguration.kt
@@ -0,0 +1,63 @@
+/*
+ * Copyright © 2019-2020 Orange.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.onap.ccsdk.cds.blueprintsprocessor.healthapi.configuration
+
+
+import org.apache.commons.net.util.Base64
+import org.springframework.stereotype.Component
+import org.springframework.stereotype.Service
+import javax.crypto.Cipher
+import javax.crypto.spec.IvParameterSpec
+import javax.crypto.spec.SecretKeySpec
+
+
+@Component
+class SecurityEncryptionConfiguration {
+    private val key = "aesEncryptionKey"
+    private val initVector = "encryptionIntVec"
+
+    fun encrypt(value: String): String? {
+        try {
+            val (iv, skeySpec, cipher) = initChiper()
+            cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv)
+            val encrypted = cipher.doFinal(value.toByteArray())
+            return Base64.encodeBase64String(encrypted)
+        } catch (ex: Exception) {
+            ex.printStackTrace()
+        }
+        return String()
+    }
+
+    open fun decrypt(encrypted: String): String? {
+        try {
+            val (iv, skeySpec, cipher) = initChiper()
+            cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv)
+            val original = cipher.doFinal(Base64.decodeBase64(encrypted))
+            return String(original)
+        } catch (ex: Exception) {
+            ex.printStackTrace()
+        }
+        return String()
+    }
+
+    private fun initChiper(): Triple<IvParameterSpec, SecretKeySpec, Cipher> {
+        val iv = IvParameterSpec(initVector.toByteArray(charset("UTF-8")))
+        val secretKeySpec = SecretKeySpec(key.toByteArray(charset("UTF-8")), "AES")
+        val cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING")
+        return Triple(iv, secretKeySpec, cipher)
+    }
+}
---
+++ ms/blueprintsprocessor/modules/inbounds/health-api/src/main/kotlin/org/onap/ccsdk/cds/blueprintsprocessor/healthapi/controller/CombinedMetrics.kt
@@ -0,0 +1,52 @@
+/*
+ * Copyright © 2019-2020 Orange.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.onap.ccsdk.cds.blueprintsprocessor.healthapi.controller
+
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
+import org.onap.ccsdk.cds.blueprintsprocessor.healthapi.domain.MetricsInfo
+import org.onap.ccsdk.cds.blueprintsprocessor.healthapi.service.CombinedMetricsService
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RestController
+
+/**
+ * Exposes API for checking Metrics of BluePrint processor and CDSListener.
+ *
+ * @author Shaaban Ebrahim
+ * @version 1.0
+ */
+@RestController
+@RequestMapping("/api/v1/combinedMetrics")
+@Api(value = "/api/v1/combinedMetrics",
+        description = "gather all Metrics info from BluePrint and CDSListener")
+open class CombinedMetrics(private val combinedMetricsService: CombinedMetricsService) {
+
+    @RequestMapping(path = [""],
+            method = [RequestMethod.GET],
+            produces = [MediaType.APPLICATION_JSON_VALUE])
+    @ResponseBody
+    @ApiOperation(value = " Metrics Check", hidden = true)
+    fun getMetricsHealthCheckResponse(): ResponseEntity<MetricsInfo?> {
+        return ResponseEntity.ok().body(combinedMetricsService.metricsInfo)
+
+    }
+
+}
