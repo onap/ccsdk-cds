37854980285619f45cc64f0ab85dbf4749cdf058
ShaabanEltanany <shaaban.eltanany.ext@orange.com>
2019-12-10 11:14:59 +0200

    adding new designer component, sort and configuration dashboard
    Issue-ID: CCSDK-1982
    Issue-ID: CCSDK-1984
    Issue-ID: CCSDK-1983
    
    Signed-off-by: ShaabanEltanany <shaaban.eltanany.ext@orange.com>
    Change-Id: I05d12bbf5ebc940bd6983da06af3f85ee18eb723

---
+++ cds-ui/designer-client/src/app/modules/feature-modules/packages/designer/functions/functions.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FunctionsComponent } from './functions.component';
+
+describe('FunctionsComponent', () => {
+  let component: FunctionsComponent;
+  let fixture: ComponentFixture<FunctionsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ FunctionsComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FunctionsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
