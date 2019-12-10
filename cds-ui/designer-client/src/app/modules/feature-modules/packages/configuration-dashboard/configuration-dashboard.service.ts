064e7443601b3bb14c669baa67665d2ed9c928d4
ShaabanEltanany <shaaban.eltanany.ext@orange.com>
2019-12-10 11:14:59 +0200

    adding new designer component, sort and configuration dashboard
    Issue-ID: CCSDK-1982
    Issue-ID: CCSDK-1984
    Issue-ID: CCSDK-1983
    
    Signed-off-by: ShaabanEltanany <shaaban.eltanany.ext@orange.com>
    Change-Id: I05d12bbf5ebc940bd6983da06af3f85ee18eb723

---
+++ cds-ui/designer-client/src/app/modules/feature-modules/packages/configuration-dashboard/configuration-dashboard.service.ts
@@ -0,0 +1,22 @@
+import {Injectable} from '@angular/core';
+import {ApiService} from '../../../../common/core/services/api.typed.service';
+import {BlueprintURLs} from '../../../../common/constants/app-constants';
+import {Observable} from 'rxjs';
+import {BluePrintDetailModel} from '../model/BluePrint.detail.model';
+
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ConfigurationDashboardService {
+
+
+    constructor(private api: ApiService<BluePrintDetailModel>) {
+
+    }
+
+    getBluePrintModel(id: string): Observable<BluePrintDetailModel> {
+        return this.api.getOne(BlueprintURLs.getOneBlueprint + '/' + id);
+
+    }
+}
