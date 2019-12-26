d67770d3f69cc81323490e2cccfd9f8640d11b9c
Brinda Santh <bs2796@att.com>
2019-12-26 15:15:27 -0500

    Cluster partition master API
    Added cluster lock close method.
    
    Added NATS connection name and default services.
    
    Junit test cases improvements.
    
    Issue-ID: CCSDK-2011
    Signed-off-by: Brinda Santh <bs2796@att.com>
    Change-Id: I44c7c1aaeae2ddbf768903152fb00bc160172fc1

--- ms/blueprintsprocessor/modules/commons/nats-lib/src/main/kotlin/org/onap/ccsdk/cds/blueprintsprocessor/nats/BluePrintNatsLibConfiguration.kt
+++ ms/blueprintsprocessor/modules/commons/nats-lib/src/main/kotlin/org/onap/ccsdk/cds/blueprintsprocessor/nats/BluePrintNatsLibConfiguration.kt
@@ -17,6 +17,7 @@
 package org.onap.ccsdk.cds.blueprintsprocessor.nats
 
 import org.onap.ccsdk.cds.blueprintsprocessor.nats.service.BluePrintNatsLibPropertyService
+import org.onap.ccsdk.cds.blueprintsprocessor.nats.service.BluePrintNatsService
 import org.onap.ccsdk.cds.controllerblueprints.core.service.BluePrintDependencyService
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
@@ -31,9 +32,14 @@ open class BluePrintNatsLibConfiguration
 fun BluePrintDependencyService.natsLibPropertyService(): BluePrintNatsLibPropertyService =
     instance(NatsLibConstants.SERVICE_BLUEPRINT_NATS_LIB_PROPERTY)
 
+fun BluePrintDependencyService.controllerNatsService(): BluePrintNatsService {
+    return natsLibPropertyService().bluePrintNatsService(NatsLibConstants.DEFULT_NATS_SELECTOR)
+}
+
 class NatsLibConstants {
     companion object {
         const val SERVICE_BLUEPRINT_NATS_LIB_PROPERTY = "blueprint-nats-lib-property-service"
+        const val DEFULT_NATS_SELECTOR = "cds-controller"
         const val PROPERTY_NATS_PREFIX = "blueprintsprocessor.nats."
         const val TYPE_TOKEN_AUTH = "token-auth"
         const val TYPE_TLS_AUTH = "tls-auth"
