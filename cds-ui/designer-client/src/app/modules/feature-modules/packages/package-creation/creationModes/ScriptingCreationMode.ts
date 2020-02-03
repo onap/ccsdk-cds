06ea5a0b453ca52e85abea6f15ca9902214e87d3
shaaban Altanany <shaaban.eltanany.ext@orange.com>
2020-02-03 16:46:57 +0200

    Package creation refactoring and save the package to CBA store.
    Issue-ID: CCSDK-2070
    
    Signed-off-by: shaaban Altanany <shaaban.eltanany.ext@orange.com>
    Change-Id: Iac67b71dbd48fe18319b381ee8a284ef9928ec46

---
+++ cds-ui/designer-client/src/app/modules/feature-modules/packages/package-creation/creationModes/ScriptingCreationMode.ts
@@ -0,0 +1,11 @@
+/*import {CBAPackage, Definition, Scripts} from '../mapping-models/CBAPacakge.model';
+import {PackageCreationModes} from './PackageCreationModes';
+import {FilesContent} from '../mapping-models/metadata/MetaDataTab.model';
+
+export class ScriptingCreationMode extends PackageCreationModes {
+    execute(cbaPackage: CBAPackage) {
+    }
+
+
+}
+*/
---
+++ cds-ui/designer-client/src/app/modules/feature-modules/packages/package-creation/mapping-models/ModeType.ts
@@ -0,0 +1,5 @@
+export enum ModeType {
+    Scripting = 'KOTLIN_SCRIPT',
+    Designer = 'DEFAULT',
+    Generic = 'GENERIC_SCRIPT'
+}
