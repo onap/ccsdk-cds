b443d07c8ee3434c83732b2a6a3b3acd0dcd5fdd
Steve Siani <alphonse.steve.siani.djissitchi@ibm.com>
2020-02-04 14:46:47 -0500

    CDS Error Catalog core library implementation
    Issue-ID: CCSDK-2076
    Committer: Alphonse Steve Siani <alphonse.steve.siani.djissitchi@ibm.com>
    Change-Id: I62382127c975d3adb6dcb02158f9f68f8cb35caa
    Signed-off-by: Steve Siani <alphonse.steve.siani.djissitchi@ibm.com>

---
+++ ms/blueprintsprocessor/functions/resource-resolution/src/test/kotlin/org/onap/ccsdk/cds/blueprintsprocessor/functions/resource/resolution/mock/MockErrorCatalogConfiguration.kt
@@ -0,0 +1,35 @@
+/*
+ *  Copyright © 2020 IBM, Bell Canada.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.onap.ccsdk.cds.blueprintsprocessor.functions.resource.resolution.mock
+
+import org.onap.ccsdk.error.catalog.data.ErrorCatalogProperties
+import org.onap.ccsdk.error.catalog.service.ErrorCatalogService
+import org.onap.ccsdk.error.catalog.service.ErrorMessagesLibPropertyService
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+open class MockErrorCatalogConfiguration {
+
+    @Bean
+    open fun errorCatalogService(): ErrorCatalogService {
+        val errorCatalogProperties = ErrorCatalogProperties()
+        errorCatalogProperties.type = "properties"
+        errorCatalogProperties.applicationId = "CDS"
+        return ErrorCatalogService(ErrorMessagesLibPropertyService(errorCatalogProperties))
+    }
+}
