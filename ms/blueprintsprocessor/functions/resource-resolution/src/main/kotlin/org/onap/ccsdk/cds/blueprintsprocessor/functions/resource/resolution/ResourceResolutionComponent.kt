9fff35e0321b2554fa3a370fdeda83e2a9615628
Steve Siani <alphonse.steve.siani.djissitchi@ibm.com>
2020-02-04 14:46:47 -0500

    CDS Error Catalog core library implementation
    Issue-ID: CCSDK-2076
    Signed-off-by: Steve Siani <alphonse.steve.siani.djissitchi@ibm.com>
    Change-Id: I4b7d6e3e912120c1b6fb86cb8a4b635ae1812bf7

--- ms/blueprintsprocessor/functions/resource-resolution/src/main/kotlin/org/onap/ccsdk/cds/blueprintsprocessor/functions/resource/resolution/ResourceResolutionComponent.kt
+++ ms/blueprintsprocessor/functions/resource-resolution/src/main/kotlin/org/onap/ccsdk/cds/blueprintsprocessor/functions/resource/resolution/ResourceResolutionComponent.kt
@@ -1,6 +1,6 @@
 /*
  * Copyright © 2017-2018 AT&T Intellectual Property.
- * Modifications Copyright © 2019 IBM.
+  * Modifications Copyright © 2019 - 2020 IBM, Bell Canada.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,7 +21,9 @@ import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.node.JsonNodeFactory
 import org.onap.ccsdk.cds.blueprintsprocessor.core.api.data.ExecutionServiceInput
 import org.onap.ccsdk.cds.blueprintsprocessor.services.execution.AbstractComponentFunction
-import org.onap.ccsdk.cds.controllerblueprints.core.BluePrintProcessorException
+import org.onap.ccsdk.cds.blueprintsprocessor.services.execution.error.BlueprintProcessorErrorCodes
+import org.onap.ccsdk.cds.blueprintsprocessor.services.execution.error.data.CDSErrorException
+import org.onap.ccsdk.cds.blueprintsprocessor.services.execution.error.data.CDSErrorImpl
 import org.onap.ccsdk.cds.controllerblueprints.core.asJsonNode
 import org.onap.ccsdk.cds.controllerblueprints.core.asObjectNode
 import org.onap.ccsdk.cds.controllerblueprints.core.returnNullIfMissing
@@ -81,15 +83,25 @@ open class ResourceResolutionComponent(private val resourceResolutionService: Re
 
         // validate inputs if we need to store the resource and template resolution.
         if (storeResult) {
+            val errorType = BlueprintProcessorErrorCodes.GENERIC_FAILURE
+            val cdsErrorImpl = CDSErrorImpl(errorType.httpCode, errorType.name)
             if (resolutionKey.isNotEmpty() && (resourceId.isNotEmpty() || resourceType.isNotEmpty())) {
-                throw BluePrintProcessorException("Can't proceed with the resolution: either provide resolution-key OR combination of resource-id and resource-type.")
+                val cdsErrorException = CDSErrorException(cdsErrorImpl, message = "Can't proceed with the " +
+                        "resolution: either provide resolution-key OR combination of resource-id and resource-type.")
+                cdsErrorException.addErrorModel(errorType)
+                throw cdsErrorException
             } else if ((resourceType.isNotEmpty() && resourceId.isEmpty()) || (resourceType.isEmpty() && resourceId.isNotEmpty())) {
-                throw BluePrintProcessorException("Can't proceed with the resolution: both resource-id and resource-type should be provided, one of them is missing.")
+                val cdsErrorException = CDSErrorException(cdsErrorImpl, message = "Can't proceed with the resolution: both resource-id and resource-type" +
+                        " should be provided, one of them is missing.")
+                cdsErrorException.addErrorModel(errorType)
+
+                throw cdsErrorException
             } else if (resourceType.isEmpty() && resourceId.isEmpty() && resolutionKey.isEmpty()) {
-                throw BluePrintProcessorException(
-                    "Can't proceed with the resolution: can't persist resolution without a correlation key. " +
-                            "Either provide a resolution-key OR combination of resource-id and resource-type OR set `storeResult` to false."
-                )
+                val cdsErrorException = CDSErrorException(cdsErrorImpl, message = "Can't proceed with the resolution: can't persist " +
+                        "resolution without a correlation key. Either provide a resolution-key OR combination of " +
+                        "resource-id and resource-type OR set `storeResult` to false.")
+                cdsErrorException.addErrorModel(errorType)
+                throw cdsErrorException
             }
         }
 
---
+++ ms/blueprintsprocessor/modules/services/execution-service/src/main/kotlin/org/onap/ccsdk/cds/blueprintsprocessor/services/execution/error/data/CDSErrorException.kt
@@ -0,0 +1,84 @@
+/*
+ *  Copyright © 2020 IBM, Bell Canada.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.onap.ccsdk.cds.blueprintsprocessor.services.execution.error.data
+
+import org.onap.ccsdk.cds.blueprintsprocessor.error.catalog.interfaces.ErrorCatalogException
+import org.onap.ccsdk.cds.blueprintsprocessor.error.catalog.interfaces.ErrorInterface
+import org.onap.ccsdk.cds.controllerblueprints.core.format
+
+open class CDSErrorImpl : ErrorInterface {
+    constructor()
+
+    constructor(code: Int) {
+        this.code = code
+    }
+    constructor(status: String) {
+        this.status = status
+    }
+    constructor(code: Int, status: String) {
+        this.code = code
+        this.status = status
+    }
+    constructor(code: Int, status: String, debugMessage: String) {
+        this.code = code
+        this.status = status
+        this.debugMessage = debugMessage
+    }
+}
+
+open class CDSErrorException : ErrorCatalogException {
+    constructor(message: String, cause: Throwable) : super(message, cause) {
+        this.errorMessage = CDSErrorImpl()
+        this.errorMessage.message = message
+        this.errorMessage.debugMessage = cause.toString()
+    }
+    constructor(message: String) : super(message) {
+        this.errorMessage = CDSErrorImpl()
+        this.errorMessage.message = message
+    }
+    constructor(cause: Throwable) : super(cause) {
+        this.errorMessage = CDSErrorImpl()
+        this.errorMessage.debugMessage = cause.toString()
+    }
+    constructor(cause: Throwable, message: String, vararg args: Any?) : super(format(message, *args), cause) {
+        this.errorMessage = CDSErrorImpl()
+        this.errorMessage.message = message
+        this.errorMessage.debugMessage = cause.toString()
+    }
+
+    constructor(errorMessage: ErrorInterface, cause: Throwable) : super(cause) {
+        this.errorMessage = errorMessage
+        this.errorMessage.debugMessage = cause.toString()
+    }
+    constructor(errorMessage: ErrorInterface, message: String) : super(message) {
+        this.errorMessage = errorMessage
+        this.errorMessage.message = message
+    }
+
+    constructor(errorMessage: ErrorInterface, message: String, cause: Throwable) : super(message, cause) {
+        this.errorMessage = errorMessage
+        this.errorMessage.message = message
+        this.errorMessage.debugMessage = cause.toString()
+    }
+
+    constructor(errorMessage: ErrorInterface, cause: Throwable, message: String, vararg args: Any?) :
+            super(String.format(message, *args), cause) {
+        this.errorMessage = errorMessage
+        this.errorMessage.message = message
+        this.errorMessage.debugMessage = cause.toString()
+    }
+}
