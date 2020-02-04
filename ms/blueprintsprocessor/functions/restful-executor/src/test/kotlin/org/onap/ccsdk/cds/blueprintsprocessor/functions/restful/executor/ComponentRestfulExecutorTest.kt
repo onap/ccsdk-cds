0ed14c1621dbb623aa39bfdc665ece10fff5cac2
Huang Cheng <duke.huangcheng@huawei.com>
2020-02-04 03:53:15 +0000

    Add the executor and the component function to support 5G NRM CM
    The executor and the component function are designed to invoke the 5G NRM CM opertions: createMOI getMOIAttributes modifyMOIAttributes and deleteMOI
    
    Issue-ID: CCSDK-2003
    Signed-off-by: Huang Cheng <duke.huangcheng@huawei.com>
    Change-Id: I20d3e97c01f6499e73ab0ec77abbc24775f428b7

---
+++ ms/blueprintsprocessor/functions/restful-executor/src/test/kotlin/org/onap/ccsdk/cds/blueprintsprocessor/functions/restful/executor/ComponentRestfulExecutorTest.kt
@@ -0,0 +1,106 @@
+/*
+ * Copyright © 2017-2018 AT&T Intellectual Property.
+ * Modifications Copyright © 2019 Huawei.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.onap.ccsdk.cds.blueprintsprocessor.functions.restful.executor
+
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.node.ObjectNode
+import io.mockk.every
+import io.mockk.mockk
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+import org.onap.ccsdk.cds.blueprintsprocessor.core.api.data.ActionIdentifiers
+import org.onap.ccsdk.cds.blueprintsprocessor.core.api.data.CommonHeader
+import org.onap.ccsdk.cds.blueprintsprocessor.core.api.data.ExecutionServiceInput
+import org.onap.ccsdk.cds.blueprintsprocessor.core.api.data.StepData
+import org.onap.ccsdk.cds.blueprintsprocessor.services.execution.ComponentFunctionScriptingService
+import org.onap.ccsdk.cds.controllerblueprints.core.BluePrintConstants
+import org.onap.ccsdk.cds.controllerblueprints.core.asJsonPrimitive
+import org.onap.ccsdk.cds.controllerblueprints.core.data.Implementation
+import org.onap.ccsdk.cds.controllerblueprints.core.service.BluePrintContext
+import org.onap.ccsdk.cds.controllerblueprints.core.service.DefaultBluePrintRuntimeService
+import org.onap.ccsdk.cds.controllerblueprints.core.utils.JacksonUtils
+import org.springframework.context.ApplicationContext
+
+class ComponentRestfulExecutorTest {
+
+    @Test
+    fun testComponentRestfulExecutor() {
+        runBlocking {
+
+            val applicationContext = mockk<ApplicationContext>()
+            every { applicationContext.getBean(any()) } returns mockk()
+
+            val componentFunctionScriptingService = ComponentFunctionScriptingService(applicationContext, mockk())
+
+            val componentRestfulExecutor = ComponentRestfulExecutor(componentFunctionScriptingService)
+
+            val executionServiceInput = ExecutionServiceInput().apply {
+                commonHeader = CommonHeader().apply {
+                    requestId = "1234"
+                }
+                actionIdentifiers = ActionIdentifiers().apply {
+                    actionName = "config-deploy"
+                }
+                payload = JacksonUtils.jsonNode("{}") as ObjectNode
+            }
+
+            val blueprintContext = mockk<BluePrintContext>()
+            every {
+                blueprintContext.nodeTemplateOperationImplementation(
+                    any(), any(), any()
+                )
+            } returns Implementation()
+
+            val bluePrintRuntime = mockk<DefaultBluePrintRuntimeService>("1234")
+            every { bluePrintRuntime.bluePrintContext() } returns blueprintContext
+
+            componentRestfulExecutor.bluePrintRuntimeService = bluePrintRuntime
+            componentRestfulExecutor.stepName = "sample-step"
+
+            val operationInputs = hashMapOf<String, JsonNode>()
+            operationInputs[BluePrintConstants.PROPERTY_CURRENT_NODE_TEMPLATE] = "config-deploy-process".asJsonPrimitive()
+            operationInputs[BluePrintConstants.PROPERTY_CURRENT_INTERFACE] = "interfaceName".asJsonPrimitive()
+            operationInputs[BluePrintConstants.PROPERTY_CURRENT_OPERATION] = "operationName".asJsonPrimitive()
+            operationInputs["script-type"] = BluePrintConstants.SCRIPT_INTERNAL.asJsonPrimitive()
+            operationInputs["script-class-reference"] = "internal.scripts.TestRestfulConfigure".asJsonPrimitive()
+
+            val stepInputData = StepData().apply {
+                name = "call-config-deploy-process"
+                properties = operationInputs
+            }
+            executionServiceInput.stepData = stepInputData
+
+            every {
+                bluePrintRuntime.resolveNodeTemplateInterfaceOperationInputs(
+                    "config-deploy-process",
+                    "interfaceName", "operationName"
+                )
+            } returns operationInputs
+
+            val operationOutputs = hashMapOf<String, JsonNode>()
+            every {
+                bluePrintRuntime.resolveNodeTemplateInterfaceOperationOutputs(
+                    "config-deploy-process",
+                    "interfaceName", "operationName"
+                )
+            } returns operationOutputs
+
+            componentRestfulExecutor.applyNB(executionServiceInput)
+        }
+    }
+}
