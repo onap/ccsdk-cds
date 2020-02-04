2dde00349fc9cfe78716107974a626978f457176
Huang Cheng <duke.huangcheng@huawei.com>
2020-02-04 03:53:15 +0000

    Add the executor and the component function to support 5G NRM CM
    The executor and the component function are designed to invoke the 5G NRM CM opertions: createMOI getMOIAttributes modifyMOIAttributes and deleteMOI
    
    Issue-ID: CCSDK-2003
    Signed-off-by: Huang Cheng <duke.huangcheng@huawei.com>
    Change-Id: I20d3e97c01f6499e73ab0ec77abbc24775f428b7

--- ms/blueprintsprocessor/functions/restful-executor/src/test/kotlin/org/onap/ccsdk/cds/blueprintsprocessor/restful/executor/nrmfunction/RestfulNRMServiceClientTest.kt
+++ ms/blueprintsprocessor/functions/restful-executor/src/test/kotlin/org/onap/ccsdk/cds/blueprintsprocessor/restful/executor/nrmfunction/RestfulNRMServiceClientTest.kt
@@ -14,7 +14,7 @@
  *  limitations under the License.
  */
 
-package org.onap.ccsdk.cds.blueprintsprocessor.functions.nrm.restful
+package org.onap.ccsdk.cds.blueprintsprocessor.functions.restful.executor.nrmfunction
 
 import com.fasterxml.jackson.databind.node.ObjectNode
 import org.onap.ccsdk.cds.controllerblueprints.core.utils.JacksonUtils
