214fca14c0ac8e4c8e102b962249056dcac35810
Brinda Santh <bs2796@att.com>
2020-03-06 13:04:37 -0500

    Error Catalog Management Core Library.
    Issue-ID: CCSDK-2076
    Signed-off-by: Brinda Santh <bs2796@att.com>
    Change-Id: I9e8e67bdee77fb359a791f4ef4c2963aa78ab93a
    Signed-off-by: Steve Siani <alphonse.steve.siani.djissitchi@ibm.com>

---
+++ ms/error-catalog/core/src/main/kotlin/org/onap/ccsdk/error/catalog/core/ErrorLibData.kt
@@ -0,0 +1,94 @@
+/*
+ *  Copyright © 2020 IBM, Bell Canada.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.onap.ccsdk.error.catalog.core
+
+import com.fasterxml.jackson.annotation.JsonFormat
+import org.slf4j.event.Level
+import org.onap.ccsdk.error.catalog.core.ErrorMessageLibConstants.ERROR_CATALOG_DEFAULT_ERROR_CODE
+import java.time.LocalDateTime
+
+open class ErrorPayload {
+    var code: Int = ERROR_CATALOG_DEFAULT_ERROR_CODE
+    var status: String = ""
+    @get:JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+    var timestamp: LocalDateTime = LocalDateTime.now()
+    var message: String = ""
+    var debugMessage: String = ""
+    var logLevel: String = Level.ERROR.name
+    val subErrors: ArrayList<ErrorMessage> = ArrayList()
+
+    constructor()
+
+    constructor(
+        code: Int = ERROR_CATALOG_DEFAULT_ERROR_CODE,
+        status: String,
+        message: String,
+        logLevel: String = Level.ERROR.name,
+        debugMessage: String = ""
+    ) {
+        this.code = code
+        this.status = status
+        this.message = message
+        this.logLevel = logLevel
+        this.debugMessage = debugMessage
+    }
+
+    constructor(
+        code: Int = ERROR_CATALOG_DEFAULT_ERROR_CODE,
+        status: String,
+        message: String,
+        logLevel: String = Level.ERROR.name,
+        debugMessage: String = "",
+        errorMessage: ErrorMessage
+    ) {
+        this.code = code
+        this.status = status
+        this.message = message
+        this.logLevel = logLevel
+        this.debugMessage = debugMessage
+        this.subErrors.add(errorMessage)
+    }
+
+    fun isEqualTo(errorPayload: ErrorPayload): Boolean {
+        return (this.code == errorPayload.code && this.status == errorPayload.status && this.message == errorPayload.message &&
+            this.logLevel == errorPayload.logLevel && this.debugMessage == errorPayload.debugMessage &&
+            this.subErrors == errorPayload.subErrors)
+    }
+}
+
+/**
+ *
+ *
+ * @author Steve Siani
+ */
+data class ErrorMessage(
+    val domainId: String,
+    val message: String,
+    val cause: String
+)
+
+data class ErrorCatalog(
+    val errorId: String,
+    val domainId: String,
+    val code: Int,
+    val action: String,
+    val cause: String
+) {
+    fun getMessage(): String {
+        return "Cause: $cause ${System.lineSeparator()} Action : $action"
+    }
+}
---
+++ ms/error-catalog/services/src/main/kotlin/org/onap/ccsdk/error/catalog/services/domain/ErrorMessageModel.kt
@@ -0,0 +1,71 @@
+/*
+ *  Copyright © 2020 IBM, Bell Canada.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.onap.ccsdk.error.catalog.services.domain
+
+import java.io.Serializable
+import java.util.UUID
+import javax.persistence.CascadeType
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.FetchType
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.Lob
+import javax.persistence.ManyToMany
+import javax.persistence.Table
+import javax.persistence.UniqueConstraint
+
+/**
+ * Provide Error Message Model Entity
+ *
+ * @author Steve Siani
+ * @version 1.0
+ */
+@Entity
+@Table(name = "ERROR_MESSAGES", uniqueConstraints = [UniqueConstraint(columnNames = ["message_id"])])
+class ErrorMessageModel : Serializable {
+
+    @Id
+    var id: String = UUID.randomUUID().toString()
+
+    @Column(name = "message_id", nullable = false)
+    lateinit var messageID: String
+
+    @Lob
+    @Column(name = "cause")
+    var cause: String = ""
+
+    @Lob
+    @Column(name = "action")
+    lateinit var action: String
+
+    @ManyToMany(mappedBy = "errorMessages", fetch = FetchType.EAGER, cascade = [CascadeType.PERSIST])
+    val domains: MutableSet<Domain> = mutableSetOf()
+
+    companion object {
+        private const val serialVersionUID = 1L
+    }
+
+    constructor()
+
+    constructor(messageId: String, cause: String, action: String) {
+        this.messageID = messageId
+        this.cause = cause
+        this.action = action
+    }
+}
