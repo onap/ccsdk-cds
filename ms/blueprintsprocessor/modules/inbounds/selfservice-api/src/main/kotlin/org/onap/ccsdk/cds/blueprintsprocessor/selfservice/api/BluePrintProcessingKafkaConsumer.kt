655b1076195cc6180df867d0e0873db7ba22931c
Julien Fontaine <julien.fontaine@bell.ca>
2020-03-23 14:33:40 -0400

    Publish execution input/output into Kafka topics
    - Creation and implementation of PublishAudit services (NoAudit,Kafka)
    - Conceal of sensitive data (request input parameters)
    - Update ExecutionServiceHandler tests
    - Update application.properties file
    
    Issue-ID: CCSDK-2183
    Signed-off-by: Julien Fontaine <julien.fontaine@bell.ca>
    Change-Id: Ifc28a91ce588f5ed0010acb4ed22b64429c6c1a0

--- ms/blueprintsprocessor/modules/inbounds/selfservice-api/src/main/kotlin/org/onap/ccsdk/cds/blueprintsprocessor/selfservice/api/BluePrintProcessingKafkaConsumer.kt
+++ ms/blueprintsprocessor/modules/inbounds/selfservice-api/src/main/kotlin/org/onap/ccsdk/cds/blueprintsprocessor/selfservice/api/BluePrintProcessingKafkaConsumer.kt
@@ -50,31 +50,21 @@ open class BluePrintProcessingKafkaConsumer(
 
     companion object {
         const val CONSUMER_SELECTOR = "self-service-api"
-        const val PRODUCER_SELECTOR = "self-service-api"
     }
 
     @EventListener(ApplicationReadyEvent::class)
     fun setupMessageListener() = runBlocking {
         try {
             log.info(
-                "Setting up message consumer($CONSUMER_SELECTOR) and " +
-                        "message producer($PRODUCER_SELECTOR)..."
+                "Setting up message consumer($CONSUMER_SELECTOR)"
             )
 
             /** Get the Message Consumer Service **/
             blueprintMessageConsumerService = try {
                 bluePrintMessageLibPropertyService
-                    .blueprintMessageConsumerService(CONSUMER_SELECTOR)
+                        .blueprintMessageConsumerService(CONSUMER_SELECTOR)
             } catch (e: Exception) {
                 throw BluePrintProcessorException("failed to create consumer service ${e.message}")
-            }
-
-            /** Get the Message Producer Service **/
-            val blueprintMessageProducerService = try {
-                bluePrintMessageLibPropertyService
-                    .blueprintMessageProducerService(PRODUCER_SELECTOR)
-            } catch (e: Exception) {
-                throw BluePrintProcessorException("failed to create producer service ${e.message}")
             }
 
             launch {
@@ -87,10 +77,8 @@ open class BluePrintProcessingKafkaConsumer(
                             ph.register()
                             log.trace("Consumed Message : $message")
                             val executionServiceInput = message.jsonAsType<ExecutionServiceInput>()
-                            val executionServiceOutput = executionServiceHandler.doProcess(executionServiceInput)
+                            executionServiceHandler.doProcess(executionServiceInput)
                             // TODO("In future, Message publisher configuration vary with respect to request")
-                            /** Send the response message */
-                            blueprintMessageProducerService.sendMessage(executionServiceOutput)
                         } catch (e: Exception) {
                             log.error("failed in processing the consumed message : $message", e)
                         } finally {
@@ -101,8 +89,7 @@ open class BluePrintProcessingKafkaConsumer(
             }
         } catch (e: Exception) {
             log.error(
-                "failed to start message consumer($CONSUMER_SELECTOR) and " +
-                        "message producer($PRODUCER_SELECTOR) ", e
+                "failed to start message consumer($CONSUMER_SELECTOR) ", e
             )
         }
     }
@@ -111,8 +98,7 @@ open class BluePrintProcessingKafkaConsumer(
     fun shutdownMessageListener() = runBlocking {
         try {
             log.info(
-                "Shutting down message consumer($CONSUMER_SELECTOR) and " +
-                        "message producer($PRODUCER_SELECTOR)..."
+                "Shutting down message consumer($CONSUMER_SELECTOR)"
             )
             blueprintMessageConsumerService.shutDown()
             ph.arriveAndAwaitAdvance()
