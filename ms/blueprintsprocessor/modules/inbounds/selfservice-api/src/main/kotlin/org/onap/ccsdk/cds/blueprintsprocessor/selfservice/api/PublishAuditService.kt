a263cdbd520786ae4b6dd80312ca2f6ee407f8c9
Julien Fontaine <julien.fontaine@bell.ca>
2020-03-23 14:33:40 -0400

    Publish execution input/output into Kafka topics
    - Creation and implementation of PublishAudit services (NoAudit,Kafka)
    - Conceal of sensitive data (request input parameters)
    - Update ExecutionServiceHandler tests
    - Update application.properties file
    
    Issue-ID: CCSDK-2183
    Signed-off-by: Julien Fontaine <julien.fontaine@bell.ca>
    Change-Id: Ifc28a91ce588f5ed0010acb4ed22b64429c6c1a0

---
+++ ms/blueprintsprocessor/modules/inbounds/selfservice-api/src/main/kotlin/org/onap/ccsdk/cds/blueprintsprocessor/selfservice/api/PublishAuditService.kt
@@ -0,0 +1,34 @@
+/*
+ * Copyright © 2020 Bell Canada
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.onap.ccsdk.cds.blueprintsprocessor.selfservice.api
+
+import org.onap.ccsdk.cds.blueprintsprocessor.core.api.data.ExecutionServiceInput
+import org.onap.ccsdk.cds.blueprintsprocessor.core.api.data.ExecutionServiceOutput
+import org.onap.ccsdk.cds.controllerblueprints.core.service.BluePrintRuntimeService
+
+interface PublishAuditService {
+    suspend fun publish(
+        correlationId: String,
+        executionServiceInput: ExecutionServiceInput,
+        bluePrintRuntimeService: BluePrintRuntimeService<*>? = null
+    )
+
+    fun publish(
+        correlationId: String,
+        executionServiceOutput: ExecutionServiceOutput
+    )
+}
