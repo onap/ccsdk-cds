42d083f7827d25d5a533cbbe5e627758613d3680
ezhil <ezhrajam@in.ibm.com>
2020-04-15 13:35:16 +0530

    Junit for SdcListenerDto
    Junit test case for SDC Listener
    Issue-ID: CCSDK-2305
    Change-Id: Ia9d4a1db86053a6f5caf2660961507f0522afd43
    Signed-off-by: ezhil <ezhrajam@in.ibm.com>

---
+++ ms/sdclistener/application/src/test/java/org/onap/ccsdk/cds/sdclistener/dto/SdcListenerDtoTest.java
@@ -0,0 +1,27 @@
+package org.onap.ccsdk.cds.sdclistener.dto;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.onap.ccsdk.cds.sdclistener.dto.SdcListenerDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@EnableConfigurationProperties(SdcListenerDto.class)
+@SpringBootTest(classes = {SdcListenerDto.class})
+public class SdcListenerDtoTest {
+
+    @Autowired
+    private SdcListenerDto listenerConfiguration;
+
+    @Test
+    public void testCdsSdcListenerDto() {
+        assertEquals(listenerConfiguration.getDistributionId(), "1234");
+        assertEquals(listenerConfiguration.getArtifactUrl(), "\"/sdc/v1/artifact\"");
+    }
+
+}
