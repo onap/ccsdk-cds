2ea4a1f874c5ba8b9c06f51329a3c1e85dd383a6
Sarah Abouzainah <sabouzainah.ext@orange.com>
2020-04-21 20:40:17 +0200

    Fix long package name and apply mapping table style
    Issue-ID: CCSDK-2321
    Issue-ID: CCSDK-2322
    
    Signed-off-by: Sarah Abouzainah <sabouzainah.ext@orange.com>
    Change-Id: I4a420ac75f341de62f4e22dfcba31d9fe1bc16c8

--- cds-ui/designer-client/src/app/modules/shared-modules/shared-modules.module.ts
+++ cds-ui/designer-client/src/app/modules/shared-modules/shared-modules.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
-
-
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
 
 @NgModule({
   declarations: [HeaderComponent],
   imports: [
-    CommonModule
-  ], exports : [HeaderComponent]
+    CommonModule,
+    TooltipModule.forRoot(),
+  ], exports : [HeaderComponent, TooltipModule,]
 })
 export class SharedModulesModule { }
