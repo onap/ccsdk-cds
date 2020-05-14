ecae390ea1619f790333e601286a35624ec2a07d
Rupinder <rupinsi1@in.ibm.com>
2020-05-14 18:13:14 +0530

    Designer > Breadcrumb
    1. Change Package name to current package name
    2. Make package name link open to package view
    
    Issue-ID: CCSDK-2247
    Change-Id: Ica060beff9a0785d80c69cf971623dd45ebe75fe
    Signed-off-by: Rupinder <rupinsi1@in.ibm.com>

--- cds-ui/designer-client/src/app/modules/feature-modules/packages/designer/designer.component.ts
+++ cds-ui/designer-client/src/app/modules/feature-modules/packages/designer/designer.component.ts
@@ -3,6 +3,8 @@
 ===================================================================
 Copyright (C) 2019 Orange. All rights reserved.
 ===================================================================
+Modification Copyright (c) 2020 IBM 
+===================================================================
 
 Unless otherwise specified, all software contained herein is licensed
 under the Apache License, Version 2.0 (the License);
@@ -34,6 +36,9 @@ import { FunctionsStore } from './functions.store';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { distinctUntilChanged } from 'rxjs/operators';
+import { BluePrintDetailModel } from '../model/BluePrint.detail.model';
+import { ActivatedRoute } from '@angular/router';
+import { DesignerService } from './designer.service';
 
 
 @Component({
@@ -46,6 +51,7 @@ export class DesignerComponent implements OnInit, OnDestroy {
 
   private controllerSideBar: boolean;
   private attributesSideBar: boolean;
+  viewedPackage: BluePrintDetailModel = new BluePrintDetailModel();
 
   boardGraph: joint.dia.Graph;
   boardPaper: joint.dia.Paper;
@@ -58,7 +64,9 @@ export class DesignerComponent implements OnInit, OnDestroy {
   constructor(private designerStore: DesignerStore,
               private functionStore: FunctionsStore,
               private graphUtil: GraphUtil,
-              private graphGenerator: GraphGenerator) {
+              private graphGenerator: GraphGenerator,
+              private route: ActivatedRoute,
+              private designerService: DesignerService) {
     this.controllerSideBar = true;
     this.attributesSideBar = false;
 
@@ -87,6 +95,14 @@ export class DesignerComponent implements OnInit, OnDestroy {
     this.initializeBoard();
     this.initializePalette();
     this.stencilPaperEventListeners();
+
+    const id = this.route.snapshot.paramMap.get('id');
+    this.designerService.getPagedPackages(id).subscribe(
+      (bluePrintDetailModels) => {
+        if (bluePrintDetailModels) {
+          this.viewedPackage = bluePrintDetailModels[0];
+        }
+      });
 
     /**
      * the code to retrieve from server is commented
--- cds-ui/designer-client/src/app/modules/feature-modules/packages/designer/source-view/source-view.component.ts
+++ cds-ui/designer-client/src/app/modules/feature-modules/packages/designer/source-view/source-view.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DesignerStore } from '../designer.store';
 import { PackageCreationUtils } from '../../package-creation/package-creation.utils';
-import { RouterLink, Router } from '@angular/router';
+import { RouterLink, Router, ActivatedRoute } from '@angular/router';
 import { Subject } from 'rxjs';
+import { BluePrintDetailModel } from '../../model/BluePrint.detail.model';
+import { viewClassName } from '@angular/compiler';
+import { SourceViewService } from './source-view.service';
 
 @Component({
     selector: 'app-designer-source-view',
@@ -15,10 +18,13 @@ export class DesignerSourceViewComponent implements OnInit, OnDestroy {
     lang = 'json';
     private controllerSideBar: boolean;
     private ngUnsubscribe = new Subject();
+    viewedPackage: BluePrintDetailModel = new BluePrintDetailModel();
 
     constructor(private store: DesignerStore,
                 private packageCreationUtils: PackageCreationUtils,
-                private router: Router) {
+                private router: Router,
+                private route: ActivatedRoute,
+                private sourceViewService: SourceViewService) {
         this.controllerSideBar = true;
     }
 
@@ -29,13 +35,20 @@ export class DesignerSourceViewComponent implements OnInit, OnDestroy {
                 this.content = this.packageCreationUtils.transformToJson(state.template);
             });
 
+        const id = this.route.snapshot.paramMap.get('id');
+        this.sourceViewService.getPagedPackages(id).subscribe(
+            (bluePrintDetailModels) => {
+                if (bluePrintDetailModels) {
+                    this.viewedPackage = bluePrintDetailModels[0];
+                }
+            });
     }
 
-    convertAndOpenInDesingerView() {
+    convertAndOpenInDesingerView(id) {
         // TODO validate json against scheme
         console.log('convertAndOpenInDesingerView ...', this.content);
         this.store.saveSourceContent(this.content);
-        this.router.navigateByUrl('/packages/designer');
+        this.router.navigate(['/packages/designer', id]);
     }
 
     ngOnDestroy() {
