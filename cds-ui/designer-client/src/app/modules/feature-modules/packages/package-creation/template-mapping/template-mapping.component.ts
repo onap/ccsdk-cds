9c94d7f0c015d4754ddc80c75db5469d2ff1e779
AhmedEldeeb50 <ahmed.eldeeb.ext@orange.com>
2020-06-21 15:55:36 +0200

    Fix editor highlight
    Issue-ID: CCSDK-2471
    
    Signed-off-by: AhmedEldeeb50 <ahmed.eldeeb.ext@orange.com>
    Change-Id: I8a78dc9c8989a933d8812c2ded7a03899800612d

--- cds-ui/designer-client/src/app/modules/feature-modules/packages/package-creation/template-mapping/template-mapping.component.ts
+++ cds-ui/designer-client/src/app/modules/feature-modules/packages/package-creation/template-mapping/template-mapping.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PackageCreationStore } from '../package-creation.store';
+import { SharedService } from './shared-service';
 
 @Component({
     selector: 'app-template-mapping',
@@ -11,17 +12,25 @@ export class TemplateMappingComponent implements OnInit {
     creationView = false;
     listView = true;
 
-    constructor(private route: ActivatedRoute, private pakcageStore: PackageCreationStore) {
+    constructor(
+        private route: ActivatedRoute,
+        private pakcageStore: PackageCreationStore,
+        private sharedService: SharedService
+    ) {
     }
 
     ngOnInit() {
+
         if (this.route.snapshot.paramMap.has('id')) {
             console.log('Edit mode');
             this.creationView = true;
             this.listView = false;
+            console.log('URL contains Id');
+            this.sharedService.enableEdit();
         } else {
             console.log('Create mode');
             this.pakcageStore.clear();
+            this.sharedService.disableEdit();
         }
     }
     openCreationView() {
