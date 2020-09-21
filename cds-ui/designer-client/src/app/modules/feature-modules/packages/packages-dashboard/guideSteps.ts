33b6dbc7a3ff9e4f197812807832494704747ae4
Sarah Abouzainah <sabouzainah.ext@orange.com>
2020-09-21 12:34:15 +0200

    Update Wizard Content
    Issue-ID: CCSDK-2705
    
    Signed-off-by: Sarah Abouzainah <sabouzainah.ext@orange.com>
    Change-Id: I57f207a49a2de99ecf137a5f15aa0260007d8ac1

--- cds-ui/designer-client/src/app/modules/feature-modules/packages/packages-dashboard/guideSteps.ts
+++ cds-ui/designer-client/src/app/modules/feature-modules/packages/packages-dashboard/guideSteps.ts
@@ -23,28 +23,28 @@ export const steps = [
     },
     {
         anchorId: 'metadataTab',
-        content: 'Set your package basic information',
+        content: 'It captures the model entities that compose the cba package name, version, description and searchable tags.',
         title: 'Metadata Tab',
         route: 'packages/createPackage'
     },
     {
         anchorId: 'mt-packageName',
-        content: 'Set your package name (required)',
+        content: 'Set your package name (required).',
         title: 'Package name',
     },
     {
         anchorId: 'mt-packageVersion',
-        content: 'Set your package version like 1.0.0 (required)',
+        content: 'Set your package version like 1.0.0 (required).',
         title: 'Package version',
     },
     {
         anchorId: 'mt-packageDescription',
-        content: 'Set your package description (required)',
+        content: 'Set your package description (required).',
         title: 'Package description',
     },
     {
         anchorId: 'mt-packageTags',
-        content: 'Set your package Tags (Optional)',
+        content: 'Set your package Tags (Optional).',
         title: 'Package tag',
     },
     // {
@@ -55,66 +55,66 @@ export const steps = [
     // Temaplate & Mapping
     {
         anchorId: 'tm-templateTab',
-        content: 'Create Your  \'Template & Mapping \' files',
+        content: 'A template is an artifact, and uses Modeling Concepts#artifact-mapping-resource and artifact-template-velocity. ',
         title: 'Temaplate & Mapping',
         stepId: 'tm-templateTab'
     },
     {
         anchorId: 'tm-templateName',
-        content: 'Set your Template & Mapping Name',
+        content: 'Set your Template & Mapping Name.',
         title: 'Temaplte & Mapping name',
         stepId: 'tm-templateName'
     },
     {
         anchorId: 'tm-templateType',
-        content: 'Set your Template Type',
+        content: 'Set your Template Type.',
         title: 'Temaplte Type',
     },
     {
         anchorId: 'tm-templateContent',
-        content: 'Click \'Import File\' to get content from a file, or write template content manually',
+        content: 'Click \'Import File\' to get content from a file, or write template content manually.',
         title: 'Template Content',
     },
     {
         anchorId: 'tm-mappingContent',
-        content: 'Set your mapping content from the current template, or from an external file (XML, CSV)',
+        content: 'Set your mapping content from the current template, or from an external file (XML, CSV).',
         title: 'Mapping Content',
     },
     {
         anchorId: 'tm-templateFinish',
-        content: 'Click your \' Finish \' button when you finish ',
+        content: 'Click your \'Finish\' button to save your template.',
         title: 'Finish',
         stepId: 'tm-templateFinish'
     },
-    {
-        anchorId: 'tm-templateEdit',
-        content: 'Create another new Template or Click on the previous one to edit',
-        title: 'Create & Edit',
-        stepId: 'tm-templateEdit'
-    },
+    // {
+    //     anchorId: 'tm-templateEdit',
+    //     content: 'Create another new Template or Click on the previous one to edit.',
+    //     title: 'Create & Edit',
+    //     stepId: 'tm-templateEdit'
+    // },
     // Script
     {
         anchorId: 'st-scriptsTab',
-        content: 'Move To Scripts Tab to set your Kotlin and Python scripts',
+        content: 'It is Kotlin/Python scripts that allows the execution of a sequence of instructions as part of CDS workflow execution.',
         title: 'Scripts',
         stepId: 'st-scriptsTab'
     },
     {
         anchorId: 'st-scriptsImport',
-        content: 'Click \' Import File\'button kotlin and python files',
+        content: 'Click to import kotlin or python files.',
         title: 'Import File'
     },
     // DSL
     {
         anchorId: 'dslTab',
-        content: 'Write your Authentication Properties in Javascript',
-        title: 'ESAP',
+        content: 'Interaction with external systems is made dynamic, removing development cycle to support new endpoint.',
+        title: 'External Systems support',
         stepId: 'dslTab'
     },
     // save package
     {
         anchorId: 'packageSave',
-        content: 'Click \' Save \' button to create your package',
+        content: 'Click to save your package.',
         title: 'Save'
     }
 ];
