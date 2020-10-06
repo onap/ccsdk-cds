0e9e7cc667e8e4925e45cfdfbbc10ef1346ae5a9
ShaabanEltanany <shaaban.eltanany.ext@orange.com>
2020-10-06 16:09:16 +0200

    adding action attributes sidebard
    Issue-ID: CCSDK-2874
    Signed-off-by: ShaabanEltanany <shaaban.eltanany.ext@orange.com>
    Change-Id: I962f98e9caaa8db4753d5266b8084eae7cb0961d

---
+++ cds-ui/designer-client/src/app/modules/feature-modules/packages/designer/action-attributes/models/InputActionAttribute.ts
@@ -0,0 +1,10 @@
+export class InputActionAttribute {
+    name: string;
+    description: string;
+    type: string;
+    required: boolean;
+}
+
+export class OutputActionAttribute extends InputActionAttribute {
+
+}
