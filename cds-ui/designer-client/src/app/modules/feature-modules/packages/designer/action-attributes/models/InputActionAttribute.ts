74239d5f1a7dbe795bfc3c221206a2530e84cf5a
ShaabanEltanany <shaaban.eltanany.ext@orange.com>
2020-11-09 13:04:44 +0200

    adding value attribute for customized output for action
    Issue-ID: CCSDK-2975
    Signed-off-by: ShaabanEltanany <shaaban.eltanany.ext@orange.com>
    Change-Id: I9a7d15943d9b3c59f0444d4b98597f6af57e3044

--- cds-ui/designer-client/src/app/modules/feature-modules/packages/designer/action-attributes/models/InputActionAttribute.ts
+++ cds-ui/designer-client/src/app/modules/feature-modules/packages/designer/action-attributes/models/InputActionAttribute.ts
@@ -4,14 +4,16 @@ export class InputActionAttribute {
     type: string;
     required: boolean;
 
+
     constructor() {
         this.name = '';
         this.description = '';
         this.type = '';
         this.required = true;
+
     }
 }
 
 export class OutputActionAttribute extends InputActionAttribute {
-
+    value = '';
 }
