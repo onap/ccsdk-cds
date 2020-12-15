0c703497d18c1f4ada56e8b0b3549820850ce637
JakobKrieg <jakob.krieg@bcmsolutions.de>
2020-12-15 13:27:39 +0100

    CDS add vFW Use Case from Integration project
    Issue-ID: DOC-698
    Change-Id: I838e90d4f229dcf544117146d72dfd1af193dfb4
    Signed-off-by: JakobKrieg <jakob.krieg@bcmsolutions.de>

--- docs/conf.py
+++ docs/conf.py
@@ -1,15 +1,18 @@
 from docs_conf.conf import *
 
 branch = 'latest'
+doc_url = 'https://docs.onap.org/projects'
 master_doc = 'index'
 
 linkcheck_ignore = [
     'http://localhost',
 ]
 
-extensions = ['sphinx_tabs.tabs', 'sphinxcontrib.swaggerdoc']
+extensions = ['sphinx_tabs.tabs', 'sphinxcontrib.swaggerdoc', 'sphinx.ext.intersphinx']
 
 intersphinx_mapping = {}
+
+intersphinx_mapping['onap-integration'] = ('{}/onap-integration/en/%s'.format(doc_url) % branch, None)
 
 html_last_updated_fmt = '%d-%b-%y %H:%M'
 
