3250ade40f01cbd4a651b8759f5bd0aceabe7c2a
Frank Kimmlingen <frank.kimmlingen@telekom.de>
2022-10-25 13:22:59 +0200

    UatExecutor does not support complex test scenarios
    Issue-ID: CCSDK-3793
    Signed-off-by: Frank Kimmlingen <frank.kimmlingen@telekom.de>
    Change-Id: I6bb03a8e03a37aa9e87d381075bc5de8cadcc7f2

---
+++ ms/blueprintsprocessor/application/src/main/kotlin/org/onap/ccsdk/cds/blueprintsprocessor/uat/utils/JsonMatcher.kt
@@ -0,0 +1,28 @@
+package org.onap.ccsdk.cds.blueprintsprocessor.uat.utils
+
+import org.mockito.ArgumentMatcher
+import org.skyscreamer.jsonassert.JSONAssert
+import org.skyscreamer.jsonassert.JSONCompareMode
+
+class JsonMatcher(val expectedJson: String?) : ArgumentMatcher<String> {
+
+    override fun matches(actualJson: String?): Boolean {
+        if (expectedJson == null) {
+            return actualJson == null
+        }
+        else if (actualJson.isNullOrEmpty() && (expectedJson.isEmpty() || expectedJson.equals("null"))) {
+            // null, "" and "null" means the same here
+            return true
+        }
+        else if (!actualJson.isNullOrEmpty() && expectedJson.isNotEmpty()) {
+            return try {
+                JSONAssert.assertEquals("", expectedJson, actualJson, JSONCompareMode.LENIENT)
+                true
+            } catch (e: AssertionError ) {
+                false
+            }
+        } else {
+            return false
+        }
+    }
+}
---
+++ ms/blueprintsprocessor/application/src/test/kotlin/org/onap/ccsdk/cds/blueprintsprocessor/uat/utils/JsonMatcherTest.kt
@@ -0,0 +1,74 @@
+package org.onap.ccsdk.cds.blueprintsprocessor.uat.utils
+
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class JsonMatcherTest {
+    @Test
+    fun `matches easy case`() {
+        val expected = """
+         {
+            "a": "b"
+         }   
+        """.trimIndent()
+        val actual = """
+         {
+            "a": "b"
+         }   
+        """.trimIndent()
+        assertTrue(JsonMatcher(expected).matches(actual))
+    }
+
+    @Test
+    fun `matches fails easy case`() {
+        val expected = """
+         {
+            "a": "b"
+         }   
+        """.trimIndent()
+        val actual = """
+         {
+            "a": "c"
+         }   
+        """.trimIndent()
+        assertFalse(JsonMatcher(expected).matches(actual))
+    }
+
+    @Test
+    fun `matches easy case (actual is lenient aka Extensible)`() {
+        val expected = """
+         {
+            "a": "b"
+         }   
+        """.trimIndent()
+        val actual = """
+         {
+            "a": "b",
+            "c": "d"
+         }   
+        """.trimIndent()
+        assertTrue(JsonMatcher(expected).matches(actual))
+
+        assertFalse(JsonMatcher(actual).matches(expected))
+    }
+
+    @Test
+    fun `matches null`() {
+        assertTrue(JsonMatcher(null).matches(null))
+    }
+
+    @Test
+    fun `matches null and "null"`() {
+        val expected : String? = null
+        val actual : String?  = null
+        assertTrue(JsonMatcher("null").matches(null))
+    }
+
+    @Test
+    fun `matches ""`() {
+        val expected : String? = null
+        val actual : String?  = null
+        assertTrue(JsonMatcher("").matches(""))
+    }
+}
