1ea82dec3f02f52764fee510972215bf72b04f3d
thmsdt <thomas.kulik@telekom.de>
2022-11-21 09:51:39 +0100

    fix doc config files
    Issue-ID: DOC-798
    
    Signed-off-by: thmsdt <thomas.kulik@telekom.de>
    Change-Id: I297dca6e628255715f2fa986b24cdec0e2db67f9

--- docs/conf.py
+++ docs/conf.py
@@ -1,20 +1,54 @@
-from docs_conf.conf import *
+project = "onap"
+release = "master"
+version = "master"
+
+author = "Open Network Automation Platform"
+# yamllint disable-line rule:line-length
+copyright = "ONAP. Licensed under Creative Commons Attribution 4.0 International License"
+
+pygments_style = "sphinx"
+html_theme = "sphinx_rtd_theme"
+html_theme_options = {
+  "style_nav_header_background": "white",
+  "sticky_navigation": "False" }
+html_logo = "_static/logo_onap_2017.png"
+html_favicon = "_static/favicon.ico"
+html_static_path = ["_static"]
+html_show_sphinx = False
+
+extensions = [
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.graphviz',
+    'sphinxcontrib.blockdiag',
+    'sphinxcontrib.seqdiag',
+    'sphinxcontrib.swaggerdoc',
+    'sphinxcontrib.plantuml',
+    'sphinx_tabs.tabs'
+]
+
+#
+# Map to 'latest' if this file is used in 'latest' (master) 'doc' branch.
+# Change to {releasename} after you have created the new 'doc' branch.
+#
 
 branch = 'latest'
+
+intersphinx_mapping = {}
 doc_url = 'https://docs.onap.org/projects'
 master_doc = 'index'
 
-linkcheck_ignore = [
-    'http://localhost',
-]
+exclude_patterns = ['.tox']
 
-extensions = ['sphinx_tabs.tabs', 'sphinxcontrib.swaggerdoc', 'sphinx.ext.intersphinx']
+spelling_word_list_filename='spelling_wordlist.txt'
+spelling_lang = "en_GB"
 
-intersphinx_mapping = {}
-
-intersphinx_mapping['onap-integration'] = ('{}/onap-integration/en/%s'.format(doc_url) % branch, None)
+intersphinx_mapping['onap-integration']    = ('{}/onap-integration/en/%s'.format(doc_url) % branch, None)
 
 html_last_updated_fmt = '%d-%b-%y %H:%M'
 
 def setup(app):
     app.add_css_file("css/ribbon.css")
+
+linkcheck_ignore = [
+  r'http://localhost:\d+/'
+]
