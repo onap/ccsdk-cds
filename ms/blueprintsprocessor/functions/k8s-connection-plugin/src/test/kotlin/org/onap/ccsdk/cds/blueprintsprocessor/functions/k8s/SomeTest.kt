06cca4bfd5a71b62c491000c8a8ef004d06053ea
Fiete Ostkamp <Fiete.Ostkamp@telekom.de>
2025-08-19 13:34:24 +0200

    sonar-cps job fails due to missing tests in k8s-connection-plugin module
    - add test to k8s-connection-plugin module because sonarqube
      fails without it
    
    Issue-ID: CCSDK-4131
    Signed-off-by: Fiete Ostkamp <Fiete.Ostkamp@telekom.de>
    Change-Id: Ia79ba6b8b25a41593b2bddf82df63b5566bd0cb6

---
+++ ms/blueprintsprocessor/functions/k8s-connection-plugin/src/test/kotlin/org/onap/ccsdk/cds/blueprintsprocessor/functions/k8s/SomeTest.kt
@@ -0,0 +1,32 @@
+/*
+ *  Copyright © 2025 Deutsche Telekom AG.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.onap.ccsdk.cds.blueprintsprocessor.functions.k8s
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+/**
+ * This placeholder of a test is necessary to make the sonar job in the pipeline not fail
+ * with a lifecycle exception because this module does not have any tests
+ */
+class SomeTest {
+
+    @Test
+    fun onlyHereToFixSonarqubeMojoExecutionException() {
+        assertTrue(true)
+    }
+}
